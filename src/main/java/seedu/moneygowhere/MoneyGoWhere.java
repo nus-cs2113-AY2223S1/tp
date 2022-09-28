c27dc95adab29f1e9cb5e5f25828bb192925ce79
xzynos <lim_fang_jie@hotmail.com>
2022-09-28 14:44:02 +0800

    Change console interface in MoneyGoWhere.java

--- src/main/java/seedu/moneygowhere/MoneyGoWhere.java
+++ src/main/java/seedu/moneygowhere/MoneyGoWhere.java
@@ -1,21 +1,20 @@
 package seedu.moneygowhere;
 
-import java.util.Scanner;
+import seedu.moneygowhere.userinterface.ConsoleInterface;
 
 public class MoneyGoWhere {
     /**
      * Main entry-point for the java.duke.Duke application.
      */
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("What is your name?");
+        ConsoleInterface consoleInterface = new ConsoleInterface();
 
-        Scanner in = new Scanner(System.in);
-        System.out.println("Hello " + in.nextLine());
+        ConsoleInterface.printLogo();
+
+        ConsoleInterface.printGreetingMessage();
+
+        consoleInterface.run();
+
+        ConsoleInterface.printGoodbyeMessage();
     }
 }
