a8bfa6983cf2c7dd91a3ad18e8dea1ff818d0325
xzynos <lim_fang_jie@hotmail.com>
2022-09-28 14:35:21 +0800

    Rename Duke to MoneyGoWhere

--- src/main/java/seedu/moneygowhere/MoneyGoWhere.java
+++ src/main/java/seedu/moneygowhere/MoneyGoWhere.java
@@ -1,8 +1,8 @@
-package seedu.duke;
+package seedu.moneygowhere;
 
 import java.util.Scanner;
 
-public class Duke {
+public class MoneyGoWhere {
     /**
      * Main entry-point for the java.duke.Duke application.
      */
