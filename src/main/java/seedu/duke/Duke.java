603d2ed373e0225ae8ea24adb869ea4f44efdf04
zhoukaiwen <e0774651@u.nus.edu>
2022-10-03 00:25:16 +0800

    Add Ui

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -6,6 +6,14 @@ public class Duke {
     /**
      * Main entry-point for the java.duke.Duke application.
      */
+    private static Ui ui;
+    public Duke() {
+        ui = new Ui();
+    }
+    private static void startDuke() {
+        new Duke();
+        ui.line();
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -17,5 +25,6 @@ public class Duke {
 
         Scanner in = new Scanner(System.in);
         System.out.println("Hello " + in.nextLine());
+        startDuke();
     }
 }
