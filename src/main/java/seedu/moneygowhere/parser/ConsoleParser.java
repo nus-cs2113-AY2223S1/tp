e337af1dc8d7df3293d430b56fd74d6f68b5b235
xzynos <lim_fang_jie@hotmail.com>
2022-10-04 23:50:46 +0800

    Add code to parse command delete-expense

--- src/main/java/seedu/moneygowhere/parser/ConsoleParser.java
+++ src/main/java/seedu/moneygowhere/parser/ConsoleParser.java
@@ -11,10 +11,12 @@ import org.apache.commons.text.matcher.StringMatcherFactory;
 import seedu.moneygowhere.commands.ConsoleCommand;
 import seedu.moneygowhere.commands.ConsoleCommandAddExpense;
 import seedu.moneygowhere.commands.ConsoleCommandBye;
+import seedu.moneygowhere.commands.ConsoleCommandDeleteExpense;
 import seedu.moneygowhere.commands.ConsoleCommandViewExpense;
 import seedu.moneygowhere.common.Configurations;
 import seedu.moneygowhere.common.Messages;
 import seedu.moneygowhere.exceptions.ConsoleParserCommandAddExpenseInvalidException;
+import seedu.moneygowhere.exceptions.ConsoleParserCommandDeleteExpenseInvalidException;
 import seedu.moneygowhere.exceptions.ConsoleParserCommandNotFoundException;
 import seedu.moneygowhere.exceptions.ConsoleParserCommandViewExpenseInvalidException;
 
@@ -31,6 +33,7 @@ public class ConsoleParser {
     public static final String CONSOLE_COMMAND_BYE = "bye";
     public static final String CONSOLE_COMMAND_ADD_EXPENSE = "add-expense";
     public static final String CONSOLE_COMMAND_VIEW_EXPENSE = "view-expense";
+    public static final String CONSOLE_COMMAND_DELETE_EXPENSE = "delete-expense";
 
     private static String[] tokenizeCommandArguments(String arguments) {
         StringTokenizer stringTokenizer = new StringTokenizer(arguments);
@@ -97,9 +100,7 @@ public class ConsoleParser {
 
             // Guard clause for mandatory arguments
             if (name == null || amountStr == null) {
-                throw new ConsoleParserCommandAddExpenseInvalidException(
-                        Messages.CONSOLE_ERROR_COMMAND_ADD_EXPENSE_INVALID
-                );
+                throw new ConsoleParserCommandAddExpenseInvalidException();
             }
 
             LocalDateTime dateTime;
@@ -111,7 +112,6 @@ public class ConsoleParser {
                         DateTimeFormatter.ofPattern(Configurations.CONSOLE_INTERFACE_DATE_TIME_INPUT_FORMAT)
                 );
             }
-
             BigDecimal amount = new BigDecimal(amountStr);
 
             return new ConsoleCommandAddExpense(
@@ -123,10 +123,10 @@ public class ConsoleParser {
         } catch (ParseException
                  | DateTimeParseException
                  | NumberFormatException
-                 | ConsoleParserCommandAddExpenseInvalidException e) {
+                 | ConsoleParserCommandAddExpenseInvalidException exception) {
             throw new ConsoleParserCommandAddExpenseInvalidException(
                     Messages.CONSOLE_ERROR_COMMAND_ADD_EXPENSE_INVALID,
-                    e
+                    exception
             );
         }
     }
@@ -158,10 +158,45 @@ public class ConsoleParser {
 
             return new ConsoleCommandViewExpense(expenseNumber);
         } catch (ParseException
-                 | NumberFormatException e) {
+                 | NumberFormatException exception) {
             throw new ConsoleParserCommandViewExpenseInvalidException(
                     Messages.CONSOLE_ERROR_COMMAND_VIEW_EXPENSE_INVALID,
-                    e
+                    exception
+            );
+        }
+    }
+
+    private static ConsoleCommandDeleteExpense parseCommandDeleteExpense(String arguments) throws
+            ConsoleParserCommandDeleteExpenseInvalidException {
+        try {
+            String[] argumentsArr = tokenizeCommandArguments(arguments);
+
+            Option optionExpenseNumber = new Option(
+                    "e",
+                    "expense-number",
+                    true,
+                    "expense number"
+            );
+            Options options = new Options();
+            options.addOption(optionExpenseNumber);
+            CommandLineParser commandLineParser = new DefaultParser();
+            CommandLine commandLine = commandLineParser.parse(options, argumentsArr);
+
+            String expenseNumberStr = commandLine.getOptionValue("expense-number");
+
+            // Guard clause for mandatory arguments
+            if (expenseNumberStr == null) {
+                throw new ConsoleParserCommandDeleteExpenseInvalidException();
+            }
+
+            int expenseNumber = Integer.parseInt(expenseNumberStr);
+
+            return new ConsoleCommandDeleteExpense(expenseNumber);
+        } catch (ParseException
+                 | NumberFormatException exception) {
+            throw new ConsoleParserCommandDeleteExpenseInvalidException(
+                    Messages.CONSOLE_ERROR_COMMAND_DELETE_EXPENSE_INVALID,
+                    exception
             );
         }
     }
@@ -171,13 +206,15 @@ public class ConsoleParser {
      *
      * @param consoleInput String read from standard input.
      * @return Parsed command and arguments
-     * @throws ConsoleParserCommandNotFoundException          If the command is not found.
-     * @throws ConsoleParserCommandAddExpenseInvalidException If the command add-expense is invalid.
+     * @throws ConsoleParserCommandNotFoundException           If the command is not found.
+     * @throws ConsoleParserCommandAddExpenseInvalidException  If the command add-expense is invalid.
+     * @throws ConsoleParserCommandViewExpenseInvalidException If the command view-expense is invalid.
      */
     public static ConsoleCommand parse(String consoleInput) throws
             ConsoleParserCommandNotFoundException,
             ConsoleParserCommandAddExpenseInvalidException,
-            ConsoleParserCommandViewExpenseInvalidException {
+            ConsoleParserCommandViewExpenseInvalidException,
+            ConsoleParserCommandDeleteExpenseInvalidException {
         String[] consoleInputArr = consoleInput.split(" ", 2);
 
         String command = consoleInputArr[0];
@@ -193,6 +230,8 @@ public class ConsoleParser {
             return parseCommandAddExpense(arguments);
         } else if (command.equalsIgnoreCase(CONSOLE_COMMAND_VIEW_EXPENSE)) {
             return parseCommandViewExpense(arguments);
+        } else if (command.equalsIgnoreCase(CONSOLE_COMMAND_DELETE_EXPENSE)) {
+            return parseCommandDeleteExpense(arguments);
         } else {
             throw new ConsoleParserCommandNotFoundException(Messages.CONSOLE_ERROR_COMMAND_NOT_FOUND);
         }
