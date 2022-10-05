138267b2f0778bb9e39283b9715f2c52a5e8a9f9
gaoyunfan <gaoyunfan8@gmail.com>
2022-10-05 18:40:52 +0800

    Update SetCommandTest

--- src/test/java/seedu/duke/command/SetCommandTest.java
+++ src/test/java/seedu/duke/command/SetCommandTest.java
@@ -1,10 +1,11 @@
 package seedu.duke.command;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import seedu.duke.Biometrics;
 import seedu.duke.Parser;
 import seedu.duke.Ui;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SetCommandTest {
 
@@ -23,19 +24,19 @@ class SetCommandTest {
         Command c = Parser.parse(fullCommand);
         c.setData(ui, biometrics);
         c.execute();
-        Assertions.assertEquals(age, biometrics.getAge());
-        Assertions.assertEquals(gender, biometrics.getGender());
-        Assertions.assertEquals(height, biometrics.getHeight());
-        Assertions.assertEquals(weight, biometrics.getWeight());
-        Assertions.assertEquals(fatPercentage, biometrics.getFatPercentage());
+        assertEquals(age, biometrics.getAge());
+        assertEquals(gender, biometrics.getGender());
+        assertEquals(height, biometrics.getHeight());
+        assertEquals(weight, biometrics.getWeight());
+        assertEquals(fatPercentage, biometrics.getFatPercentage());
     }
 
     @Test
-    void execute_InvalidSetArguments_UnchangedBiometrics() {
+    void execute_NegativeInt_UnchangedBiometrics() {
         Biometrics biometrics = new Biometrics();
         Ui ui = new Ui();
         String command = "set biometrics";
-        int age = 22;
+        int age = -11;
         String gender = "male";
         int height = 172;
         int weight = 70;
@@ -45,10 +46,32 @@ class SetCommandTest {
         Command c = Parser.parse(fullCommand);
         c.setData(ui, biometrics);
         c.execute();
-        Assertions.assertEquals(0, biometrics.getAge());
-        Assertions.assertEquals("-", biometrics.getGender());
-        Assertions.assertEquals(0, biometrics.getHeight());
-        Assertions.assertEquals(0, biometrics.getWeight());
-        Assertions.assertEquals(0, biometrics.getFatPercentage());
+        assertEquals(0, biometrics.getAge());
+        assertEquals("-", biometrics.getGender());
+        assertEquals(0, biometrics.getHeight());
+        assertEquals(0, biometrics.getWeight());
+        assertEquals(0, biometrics.getFatPercentage());
+    }
+
+    @Test
+    void execute_InvalidGender_UnchangedBiometrics() {
+        Biometrics biometrics = new Biometrics();
+        Ui ui = new Ui();
+        String command = "set biometrics";
+        int age = 22;
+        String gender = "oher";
+        int height = 172;
+        int weight = 70;
+        int fatPercentage = 22;
+        String fullCommand = String.format("%s %d %s %d %d %d", command, age, gender, height, weight, fatPercentage);
+
+        Command c = Parser.parse(fullCommand);
+        c.setData(ui, biometrics);
+        c.execute();
+        assertEquals(0, biometrics.getAge());
+        assertEquals("-", biometrics.getGender());
+        assertEquals(0, biometrics.getHeight());
+        assertEquals(0, biometrics.getWeight());
+        assertEquals(0, biometrics.getFatPercentage());
     }
 }
