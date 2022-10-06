1eb21d06901e6d2cab6b0fed5408d1bd8cba38d1
Matthew Phua <matthew.phua@icloud.com>
2022-10-06 15:38:15 +0800

    adding

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -7,8 +7,9 @@ public class Duke {
      * Main entry-point for the java.duke.Duke application.
      */
     public static void main(String[] args) {
+        // test change
         String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____   \n"
+                + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
