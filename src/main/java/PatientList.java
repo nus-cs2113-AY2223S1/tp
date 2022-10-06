4c567198959181f4d7675ff4ae58abc3de183e2e
dhanish265 <dhanish265@gmail.com>
2022-10-06 17:40:25 +0800

    restructure patientList class by making variables and methods non-static

--- src/main/java/PatientList.java
+++ src/main/java/PatientList.java
@@ -1,10 +1,14 @@
 import java.util.ArrayList;
 
 public class PatientList {
-    public static ArrayList<Patient> patients = new ArrayList<>();
+    public ArrayList<Patient> patients;
 
-    public static void addPatient(String name, String birthDate,String gender) {
-        patients.add(new Patient(name,birthDate,gender));
+    public PatientList() {
+        patients = new ArrayList<>();
+    }
+
+    public void addPatient(String name, String birthDate, String gender) {
+        patients.add(new Patient(name, birthDate, gender));
     }
 
 }
