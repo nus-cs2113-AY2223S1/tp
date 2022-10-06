021ad225dbb2877fe94e5e858ec534fbf69428df
Koh Ngiap Hin <64647635+kohnh@users.noreply.github.com>
2022-10-07 00:56:56 +0800

    Create barebones Parser class

--- src/main/java/seedu/duke/parser/Parser.java
+++ src/main/java/seedu/duke/parser/Parser.java
@@ -1,5 +1,23 @@
 package seedu.duke.parser;
 
-public class Parser {
+import seedu.duke.command.*;
 
+public class Parser {
+    public static Command parse(String userInput) {
+        String[] keywords = userInput.split("\\s+");
+        switch (keywords[0]) {
+        case ("search"):
+            return new SearchModulesCommand();
+        case ("add"):
+            return new AddModuleCommand();
+        case ("delete"):
+            return new DeleteModuleCommand();
+        case ("view"):
+            return new ViewTimetableCommand();
+        case ("help"):
+            return new HelpCommand();
+        default:
+            return new UnknownCommand();
+        }
+    }
 }
