9e7535d7d5166a8b3930085f3d9c9cdb8dacee8c
Koh Ngiap Hin <64647635+kohnh@users.noreply.github.com>
2022-10-07 01:15:48 +0800

    Fix checkstyle error for Parser

--- src/main/java/seedu/duke/parser/Parser.java
+++ src/main/java/seedu/duke/parser/Parser.java
@@ -1,6 +1,12 @@
 package seedu.duke.parser;
 
-import seedu.duke.command.*;
+import seedu.duke.command.Command;
+import seedu.duke.command.AddModuleCommand;
+import seedu.duke.command.DeleteModuleCommand;
+import seedu.duke.command.HelpCommand;
+import seedu.duke.command.SearchModulesCommand;
+import seedu.duke.command.ViewTimetableCommand;
+import seedu.duke.command.UnknownCommand;
 
 public class Parser {
     public static Command parse(String userInput) {
