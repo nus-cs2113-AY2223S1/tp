06a806a20766bb35e4dfa462a29e32dafd88df1c
dhanish265 <dhanish265@gmail.com>
2022-10-06 20:24:37 +0800

    Add features to retrieve and print out the details of a patient given an ID

--- src/main/java/PatientList.java
+++ src/main/java/PatientList.java
@@ -11,4 +11,19 @@ public class PatientList {
         patients.add(new Patient(name, birthDate, gender, id));
     }
 
+    public void retrievePatient(String id) {
+        for (Patient patient: patients) {
+            if (patient.getId().equals(id)) {
+                Messages.printLine();
+                System.out.println("The patient with the supplied ID was found! Here are the details of the patient: ");
+                System.out.println(patient);
+                Messages.printLine();
+                return;
+            }
+        }
+        Messages.printLine();
+        System.out.println("Sorry, no patient exists with the supplied ID!");
+        Messages.printLine();
+    }
+
 }
