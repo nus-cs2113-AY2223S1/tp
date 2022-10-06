e5f78d2f6b321901b05a2c1d1898e0d0c38281e7
nazrul0 <nazrulsyahmi00@gmail.com>
2022-10-06 16:26:41 +0800

    copied redders' files to resolve CI

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -3,9 +3,7 @@ package seedu.duke;
 import java.util.Scanner;
 
 public class Duke {
-    /**
-     * Main entry-point for the java.duke.Duke application.
-     */
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
