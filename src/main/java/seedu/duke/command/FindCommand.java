9ed58f9346d7ef6207c52dccd94e342a0a12eb4b
wcwy <thinhong99@gmail.com>
2022-10-08 04:46:47 +0800

    Add static strings on command help info template for Find and Get

--- src/main/java/seedu/duke/command/FindCommand.java
+++ src/main/java/seedu/duke/command/FindCommand.java
@@ -12,6 +12,17 @@ import static seedu.duke.common.InfoMessages.INFO_LIST_UNFILTERED;
  * Represents a find command object that will execute the operations for Find command.
  */
 public class FindCommand extends Command {
+
+    // The command word used to trigger the execution of Moolah Manager's operations.
+    public static final String COMMAND_WORD = "FIND";
+    // The description for the usage of command.
+    public static final String COMMAND_DESCRIPTION = "To find ...";
+    // The guiding information for the usage of command.
+    public static final String COMMAND_USAGE = "Usage: find ...";
+    // The formatting information for the parameters used by the command.
+    public static final String COMMAND_PARAMETERS_INFO = "Parameters information: \n"
+            + "PARAMETERNAME: ... ";
+
     protected String keywords;
 
     /**
@@ -38,9 +49,9 @@ public class FindCommand extends Command {
     /**
      * Executes the operations related to the command.
      *
-     * @param ui An instance of the Ui class.
+     * @param ui           An instance of the Ui class.
      * @param transactions An instance of the TransactionList class.
-     * @param storage An instance of the Storage class.
+     * @param storage      An instance of the Storage class.
      */
     @Override
     public void execute(TransactionList transactions, Ui ui, Storage storage) {
