2707fd674ae8dea792e8a2d50b9a83b216b45cdb
Owen Leong <owenl131@gmail.com>
2022-10-07 20:24:20 +0800

    Add lesson type as an enum

--- src/main/java/seedu/duke/model/SemesterData.java
+++ src/main/java/seedu/duke/model/SemesterData.java
@@ -1,6 +1,9 @@
 package seedu.duke.model;
 
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 public class SemesterData {
     public final int semester;
@@ -13,5 +16,26 @@ public class SemesterData {
         this.timetable = timetable;
         this.examDate = examDate;
         this.examDuration = examDuration;
+    }
+
+    public Set<LessonType> lessonTypes() {
+        Set<LessonType> set = new TreeSet<>();
+        for (RawLesson lesson : timetable) {
+            set.add(lesson.lessonType);
+        }
+        return set;
+    }
+
+    public List<RawLesson> getLessonByTypeAndNo(LessonType type, String no) {
+        return timetable.stream().filter(lesson -> lesson.lessonType == type && lesson.classNo.equals(no))
+                .collect(Collectors.toList());
+    }
+
+    public List<RawLesson> getLessonByNo(String no) {
+        return timetable.stream().filter(lesson -> lesson.classNo.equals(no)).collect(Collectors.toList());
+    }
+
+    public List<RawLesson> getLessonsByType(LessonType type) {
+        return timetable.stream().filter(lesson -> lesson.lessonType.equals(type)).collect(Collectors.toList());
     }
 }
