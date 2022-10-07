6ef6c0fddecb2d6da4e222a05525647a207aebba
wcwy <thinhong99@gmail.com>
2022-10-08 04:24:56 +0800

    Set up the command word, description, usage and parameters info for each command.
    Change the command word used in Parser.java to use the static command
    word specified in each Command class.

--- src/main/java/seedu/duke/command/Command.java
+++ src/main/java/seedu/duke/command/Command.java
@@ -16,6 +16,17 @@ public abstract class Command {
      * @param transactions An instance of the TransactionList class.
      * @param storage An instance of the Storage class.
      */
+
+    // The command word used to trigger the execution of Moolah Manager's operations.
+    public static String COMMAND_WORD;
+    // The description for the usage of command.
+    public static String COMMAND_DESCRIPTION;
+    // The guiding information for the usage of command.
+    public static String COMMAND_USAGE;
+    // The formatting information for the parameters used by the command.
+    public static String COMMAND_PARAMETERS_INFO;
+
+
     public abstract void execute(TransactionList transactions, Ui ui, Storage storage) throws MoolahException;
 
     public abstract boolean isExit();
