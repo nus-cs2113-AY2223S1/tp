ff5115af1cbf656dc361a1c26cff36222d0cdb29
CheahHaoYi <haoyicheah@gmail.com>
2022-10-07 23:12:28 +0800

    Add some trivial JUnit test code

--- src/test/java/seedu/duke/DukeTest.java
+++ src/test/java/seedu/duke/DukeTest.java
@@ -1,6 +1,7 @@
 package seedu.duke;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
@@ -8,5 +9,10 @@ class DukeTest {
     @Test
     public void sampleTest() {
         assertTrue(true);
+    }
+
+    @Test
+    public void sampleTest2() {
+        assertEquals(2,2);
     }
 }
