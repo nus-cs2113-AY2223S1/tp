8e53b6fa6015ef097b2fb7b2ce39768e377822c6
Nay <naychi.w_naing@u.yale-nus.edu.sg>
2022-10-07 17:18:00 +0800

    Changes to comply coding standard

--- src/main/java/seedu/duke/command/AddCommand.java
+++ src/main/java/seedu/duke/command/AddCommand.java
@@ -9,11 +9,13 @@ import seedu.duke.exception.IllegalValueException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class AddCommand extends Command{
+public class AddCommand extends Command {
     private Ui ui;
     private String arguments;
 
     private Food food;
+    public static ArrayList<Food> foodList = new ArrayList<>();
+    public static final String INVALID_FOOD_INPUT = "Invalid food input";
 
     @Override
     public void execute() {
@@ -25,9 +27,7 @@ public class AddCommand extends Command{
     public AddCommand(String arguments) {
         this.arguments = arguments;
     }
-    public static ArrayList<Food> foodList = new ArrayList<>();
 
-    public static final String INVALID_FOOD_INPUT = "Invalid food input";
 
     private void addFood(String[] argumentList) {
         try {
@@ -37,13 +37,13 @@ public class AddCommand extends Command{
             String description = argumentList[1];
             int calories = Integer.parseInt(argumentList[2]);
 
-            if ( description.equals("") || calories <= 0) {
+            if (description.equals("") || calories <= 0) {
                 throw new IllegalValueException(INVALID_FOOD_INPUT);
             }
-            food = new Food (description, calories);
+            food = new Food(description, calories);
             foodList.add(food);
             ui.output(food.toString());
-            ui.output (" This food is added to the food list successfully");
+            ui.output(" This food is added to the food list successfully");
         } catch (NumberFormatException e) {
             ui.output(INVALID_FOOD_INPUT);
         } catch (IllegalValueException e) {
