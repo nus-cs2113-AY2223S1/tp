250677d21fa1a2bb65b4e42881b3ebcca531ceb6
Brominne <e0773671@u.nus.edu>
2022-10-08 13:55:19 +0800

    update List command

--- src/main/java/recipeditor/command/ListCommand.java
+++ src/main/java/recipeditor/command/ListCommand.java
@@ -1,14 +1,23 @@
 package recipeditor.command;
 
+import recipeditor.recipe.Recipe;
+import recipeditor.recipe.RecipeList;
+
 public class ListCommand extends Command {
     public static final String TYPE = "list";
 
-
-
+    /**
+     * execute list command, store all recipes in a string in the
+     * format of <index> <recipe descreption>
+     * @return a compiled list of all recipes
+     */
     public CommandResult execute() {
-        //TODO: Execution of command
-        // This is only the dummy code for testing
-        return new CommandResult(TYPE);
+        String result = "";
+        int i = 1;
+        for(Recipe recipe : RecipeList.getRecipes()) {
+            result += String.format("%n%d. %s", i++, recipe.toString());
+        }
+        return new CommandResult(result);
     }
 }
 
