0fe5200ff4eb8d50f2c1f4bcafeb47418fbc42bd
xzynos <lim_fang_jie@hotmail.com>
2022-10-08 20:52:40 +0800

    Add console messages when expense is not found

--- src/main/java/seedu/moneygowhere/common/Messages.java
+++ src/main/java/seedu/moneygowhere/common/Messages.java
@@ -4,10 +4,14 @@ package seedu.moneygowhere.common;
  * Defines the messages used by program.
  */
 public class Messages {
-    public static final String CONSOLE_MESSAGE_GREETING = "Your MoneyGoWhere? Let me help you find it.";
-    public static final String CONSOLE_MESSAGE_GOODBYE = "Bye. Hope to see you again soon!";
-    public static final String CONSOLE_ERROR_COMMAND_NOT_FOUND = "The command entered is invalid.";
-    public static final String CONSOLE_MESSAGE_COMMAND_ADD_EXPENSE_SUCCESS = "The expense was added successfully.";
+    public static final String CONSOLE_MESSAGE_GREETING = ""
+            + "Your MoneyGoWhere? Let me help you find it.";
+    public static final String CONSOLE_MESSAGE_GOODBYE = ""
+            + "Bye. Hope to see you again soon!";
+    public static final String CONSOLE_ERROR_COMMAND_NOT_FOUND = ""
+            + "The command entered is invalid.";
+    public static final String CONSOLE_MESSAGE_COMMAND_ADD_EXPENSE_SUCCESS = ""
+            + "The expense was added successfully.";
     public static final String CONSOLE_ERROR_COMMAND_ADD_EXPENSE_INVALID = ""
             + "The arguments entered are invalid. "
             + "SYNTAX: Add-Expense -n NAME -a AMOUNT [-d "
@@ -16,18 +20,22 @@ public class Messages {
     public static final String CONSOLE_ERROR_COMMAND_VIEW_EXPENSE_INVALID = ""
             + "The arguments entered are invalid. "
             + "SYNTAX: View-Expense [-e EXPENSE_NUMBER]";
-    public static final String CONSOLE_MESSAGE_COMMAND_DELETE_EXPENSE_SUCCESS = "The expense was deleted successfully.";
+    public static final String CONSOLE_MESSAGE_COMMAND_DELETE_EXPENSE_SUCCESS = ""
+            + "The expense was deleted successfully.";
     public static final String CONSOLE_ERROR_COMMAND_DELETE_EXPENSE_INVALID = ""
             + "The arguments entered are invalid. "
             + "SYNTAX: Delete-Expense [-e EXPENSE_NUMBER]";
-    public static final String CONSOLE_MESSAGE_COMMAND_EDIT_EXPENSE_SUCCESS = "The expense was edited successfully.";
+    public static final String CONSOLE_MESSAGE_COMMAND_EDIT_EXPENSE_SUCCESS = ""
+            + "The expense was edited successfully.";
     public static final String CONSOLE_ERROR_COMMAND_EDIT_EXPENSE_INVALID = ""
             + "The arguments entered are invalid. "
             + "SYNTAX: Edit-Expense -e EXPENSE_NUMBER [-n NAME] [-a AMOUNT] [-d "
             + Configurations.CONSOLE_INTERFACE_DATE_TIME_INPUT_FORMAT
             + "] [-t DESCRIPTION] [-c CATEGORY]";
-    public static final String CONSOLE_MESSAGE_COMMAND_SORTED_EXPENSE_SUCCESS =
-            "Your expenses have been sorted successfully";
+    public static final String EXPENSE_MANAGER_ERROR_EXPENSE_NOT_FOUND = ""
+            + "The expense is not found.";
+    public static final String CONSOLE_MESSAGE_COMMAND_SORTED_EXPENSE_SUCCESS = ""
+            + "Your expenses have been sorted successfully";
     public static final String CONSOLE_ERROR_COMMAND_SORT_EXPENSE_INVALID = ""
             + "The arguments entered are invalid. "
             + "SYNTAX: Sort-Expense -t TYPE -o ORDER";
@@ -38,7 +46,8 @@ public class Messages {
             + "There is an error in load file found...\n"
             + "Please ensure the file is the correct load file without modifications to it.\n"
             + "Error is found in line: ";
-    public static final String LOCAL_STORAGE_LOAD_SUCCESS = "File loaded successfully :)";
+    public static final String LOCAL_STORAGE_LOAD_SUCCESS = ""
+            + "File loaded successfully :)";
     public static final String LOCAL_STORAGE_ERROR_SAVE_DATA = ""
             + "There is an error in saving file...\n"
             + "File does not exist or Path is wrong :(";
