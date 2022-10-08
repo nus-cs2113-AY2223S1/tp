92319823621772b32698b876d3b54e0c8a63e40d
Franky4566 <rachelfong6@gmail.com>
2022-10-08 22:43:34 +0800

    Modify Duke file input

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -3,10 +3,40 @@ package seedu.duke;
 import java.util.Scanner;
 
 public class Duke {
+
+    static boolean isFinished = true;
     /**
      * Main entry-point for the java.duke.Duke application.
      */
     public static void main(String[] args) {
+        welcomeMessage();
+
+        Scanner sc = new Scanner(System.in);
+        while (isFinished) {
+            String command = sc.nextLine();
+            if(readInput(command) == false) {
+                break;
+            }
+        }
+        goodbyeMessage();
+
+    }
+
+    public static boolean readInput(String command) {
+
+        String[] splitCommand = command.split(" ");
+        switch (splitCommand[0]) {
+        case "bye":
+            isFinished = false;
+            return isFinished;
+        case "flight_add":
+            break;
+        }
+
+        return isFinished;
+    }
+
+    public static void welcomeMessage() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -15,7 +45,9 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("What is your name?");
 
-        Scanner in = new Scanner(System.in);
-        System.out.println("Hello " + in.nextLine());
+    }
+
+    public static void goodbyeMessage() {
+        System.out.println("Thank you, come again! :)\n");
     }
 }
