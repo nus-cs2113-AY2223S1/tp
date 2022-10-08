0f7a9c77569ccb442c8a5d1a29b17eb3efcdb488
snuckerzlol <williamwahyudiwilliam@gmail.com>
2022-10-08 09:33:53 +0800

    Recipes

--- src/main/java/recipeditor/recipe/RecipeList.java
+++ src/main/java/recipeditor/recipe/RecipeList.java
@@ -1,5 +1,32 @@
 package recipeditor.recipe;
 
+import java.util.ArrayList;
+
 public class RecipeList {
-    
+    private static ArrayList<Recipe> recipes;
+
+    public RecipeList(ArrayList<Recipe> load) {
+        recipes = new ArrayList<>();
+        recipes.addAll(load);
+    }
+
+    public RecipeList() {
+        this(null);
+    }
+
+    public ArrayList<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public Recipe getRecipe(int index) {
+        return recipes.get(index);
+    }
+
+    public void addRecipe(Recipe recipe) {
+        recipes.add(recipe);
+    }
+
+    public void deleteRecipe(int index) {
+        recipes.remove(index);
+    }
 }
