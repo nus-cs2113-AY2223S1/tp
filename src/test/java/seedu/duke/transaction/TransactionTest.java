4aef3e2e511176b27168336646a4cf08a99babf7
bdthanh <bdthanh200403@gmail.com>
2022-10-09 16:53:49 +0800

    Modify Transaction

--- src/test/java/seedu/duke/transaction/TransactionTest.java
+++ src/test/java/seedu/duke/transaction/TransactionTest.java
@@ -1,10 +1,10 @@
 package seedu.duke.transaction;
 
 import org.junit.jupiter.api.Test;
-import seedu.duke.item.Item;
-import seedu.duke.user.User;
+import seedu.duke.parser.DateParser;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -14,49 +14,35 @@ class TransactionTest {
 
     @Test
     void setAsFinished_expectTrue() {
-        User lender = new User("John Doe", 45, "93746378");
-        User borrower = new User("Doe John", 23, "12312378");
-        Item item = new Item("Broom", 1, 0.5, lender);
-        Transaction transaction = new Transaction(item, borrower, 5, "2022-01-03");
+        Transaction transaction = new Transaction("6650266082", "6650266082", 5, "2022-10-03");
         transaction.setAsFinished();
         assertTrue(transaction.isFinished());
     }
 
     @Test
     void setAsNotFinished_expectFalse() {
-        User lender = new User("John Doe", 45, "93746378");
-        User borrower = new User("Doe John", 23, "12312378");
-        Item item = new Item("Broom", 1, 0.5, lender);
-        Transaction transaction = new Transaction(item, borrower, 5, "2022-01-03");
+        Transaction transaction = new Transaction("6650266082", "6650266082", 5, "2022-10-03");
         transaction.setAsNotFinished();
         assertFalse(transaction.isFinished());
     }
 
     @Test
     void getReturnDate_fiveDaysDuration_expectReturnDate5DaysFromCreatedDate() {
-        User lender = new User("John Doe", 45, "93746378");
-        User borrower = new User("Doe John", 23, "12312378");
-        Item item = new Item("Broom", 1, 0.5, lender);
-        Transaction transaction = new Transaction(item, borrower, 5, "2022-01-03");
+        Transaction transaction = new Transaction("6650266082", "6650266082", 5, "2022-01-03");
         assertEquals(LocalDate.parse("2022-01-08"), transaction.getReturnDate());
     }
 
     @Test
     void isOverdue_isNotFinished_expectTrue() {
-        User lender = new User("John Doe", 45, "93746378");
-        User borrower = new User("Doe John", 23, "12312378");
-        Item item = new Item("Broom", 1, 0.5, lender);
-        Transaction transaction = new Transaction(item, borrower, 5, "2022-01-03");
+        Transaction transaction = new Transaction("6650266082", "6650266082", 5, "2022-10-03");
         assertTrue(transaction.isOverdue());
     }
 
     @Test
     void isOverdue_isFinished_expectFalse() {
-        User lender = new User("John Doe", 45, "93746378");
-        User borrower = new User("Doe John", 23, "12312378");
-        Item item = new Item("Broom", 1, 0.5, lender);
-        Transaction transaction = new Transaction(item, borrower, 5, "2022-01-03");
+        Transaction transaction = new Transaction("6650266082", "6650266082", 5, "2022-10-03");
         transaction.setAsFinished();
         assertFalse(transaction.isOverdue());
     }
+
 }
