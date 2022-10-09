cee79c56bfc3621e8a8ecee7005fe22a3411a581
zhoukaiwen <e0774651@u.nus.edu>
2022-10-09 23:59:01 +0800

    Add more Parser Tests

--- src/test/java/seedu/duke/ParserTest.java
+++ src/test/java/seedu/duke/ParserTest.java
@@ -5,7 +5,8 @@ import seedu.duke.command.Command;
 import seedu.duke.command.ExitCommand;
 import seedu.duke.command.InvalidCommand;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ParserTest {
 
@@ -23,4 +24,15 @@ public class ParserTest {
         assertTrue(command instanceof InvalidCommand);
     }
 
+    @Test
+    void getArgumentList_FiveSeparators_SixArguments() {
+        String userInput = "arg 1 /arg 2 /arg3 / arg4/  arg 5/arg6";
+        String[] argumentList = Parser.getArgumentList(userInput);
+        assertEquals("arg 1 ", argumentList[0]);
+        assertEquals("arg 2 ", argumentList[1]);
+        assertEquals("arg3 ", argumentList[2]);
+        assertEquals(" arg4", argumentList[3]);
+        assertEquals("  arg 5", argumentList[4]);
+        assertEquals("arg6", argumentList[5]);
+    }
 }
