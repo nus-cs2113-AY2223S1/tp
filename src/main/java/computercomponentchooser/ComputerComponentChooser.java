a753bf18a8fcc1f727d38a00238f3b269c527097
PotatoChips <69447339+P0tatoChips@users.noreply.github.com>
2022-10-09 21:16:26 +0800

    Moved methods and minor edits
    Moved line reading to Ui.
    Edited some methods to be similar to those in EditParser.

--- src/main/java/computercomponentchooser/ComputerComponentChooser.java
+++ src/main/java/computercomponentchooser/ComputerComponentChooser.java
@@ -6,20 +6,13 @@ public class ComputerComponentChooser {
     public static void main(String[] args) {
         Ui.startSession();
 
-        String line;
         Scanner in = new Scanner(System.in);
 
         BuildManager buildManager = new BuildManager();
 
-        do {
-            line = in.nextLine();
-            if (Parser.checkEdit(line)) {
-                while (!EditParser.checkBack(line)) {
-                    line = in.nextLine();
-                }
-            }
-        } while (!Parser.checkBye(line));
+        Ui.readLine(in);
 
         Ui.endSession();
     }
+
 }
