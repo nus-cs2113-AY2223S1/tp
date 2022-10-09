44a1ee2cc8c6c4c570a54b3ad456ffe830bba8b7
Franky4566 <rachelfong6@gmail.com>
2022-10-09 17:03:39 +0800

    Create FlightManager class

---
+++ src/main/java/FlightManager.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class FlightManager {
+    public static ArrayList<String> flightList  = new ArrayList<>();
+
+//  1. Flight number fn/
+//  2. Airline a/
+//  3. Flight destination d/
+//  4. Flight departure timing t/
+//  5. Boarding gate number gn/
+//  6. Terminal location tl/
+//  7. Check in row/door c/
+
+    public static void addFlight(String input) {
+
+    }
+
+}
