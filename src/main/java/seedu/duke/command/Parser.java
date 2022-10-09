39cf7b57af420f95d7212ac6b50fb6bf5aff844a
joshuan98 <89023959+joshuan98@users.noreply.github.com>
2022-10-09 16:34:36 +0800

    Add assert and logging for parser

--- src/main/java/seedu/duke/command/Parser.java
+++ src/main/java/seedu/duke/command/Parser.java
@@ -1,26 +1,50 @@
 package seedu.duke.command;
 
-import seedu.duke.exceptions.InvalidDatabaseLineException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Parser {
-    public static String[] parseDatabaseLine(String line) throws InvalidDatabaseLineException {
+    private static Logger logger = Logger.getLogger("Parser");
+
+    public static String[] parseDatabaseLine(String line) {
+        logger.log(Level.FINE, "Start parsing database line");
+
         String[] parts = line.split(",");
 
-        if (parts.length != 15) {
-            throw new InvalidDatabaseLineException();
-        }
+        assert parts.length == 15 : "Database line should have 15 segments";
 
         String partnerUniversity = parts[1];
-        String parterUniversityModuleCode = parts[2];
-        String partnerUnviersityModuleTitle = parts[3];
-        String partnerUniversityModuleCredit = parts[4];
-        String nusModuleCode = parts[8];
-        String nusModuleTitle = parts[9];
-        String nusModuleCredit = parts[10];
+        assert partnerUniversity.length() > 0 : "Partner University should not be empty";
 
-        String[] lineData = { partnerUniversity, parterUniversityModuleCode, partnerUnviersityModuleTitle,
-            partnerUniversityModuleCredit, nusModuleCode,
-            nusModuleTitle, nusModuleCredit };
+        String parterUniversityModuleCode = parts[2];
+        assert parterUniversityModuleCode.length() > 0
+                : "Partner University Module Code should not be empty";
+
+        String partnerUnviersityModuleTitle = parts[3];
+        assert partnerUnviersityModuleTitle.length() > 0
+                : "Partner University Module Title should not be empty";
+
+        String partnerUniversityModuleCredit = parts[4];
+        assert partnerUniversityModuleCredit.length() > 0
+                : "Partner University Module Credit should not be empty";
+
+        String nusModuleCode = parts[8];
+        assert nusModuleCode.length() > 0
+                : "NUS Module Code should not be empty";
+
+        String nusModuleTitle = parts[9];
+        assert nusModuleTitle.length() > 0
+                : "NUS Module Title should not be empty";
+
+        String nusModuleCredit = parts[10];
+        assert nusModuleCredit.length() > 0
+                : "NUS Module Credit should not be empty";
+
+        String[] lineData = { partnerUniversity, parterUniversityModuleCode,
+                partnerUnviersityModuleTitle, partnerUniversityModuleCredit, nusModuleCode,
+                nusModuleTitle, nusModuleCredit };
+
+        logger.log(Level.FINE, "End parsing database line");
 
         return lineData;
     }
