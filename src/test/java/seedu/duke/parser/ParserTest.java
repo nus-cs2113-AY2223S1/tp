c7b59cdf93afd1a73f94d5f649f85830e073ccd7
Koh Ngiap Hin <64647635+kohnh@users.noreply.github.com>
2022-10-09 17:14:57 +0800

    Add Parsing for Exit and SearchString commands

--- src/test/java/seedu/duke/parser/ParserTest.java
+++ src/test/java/seedu/duke/parser/ParserTest.java
@@ -3,10 +3,12 @@ package seedu.duke.parser;
 import org.junit.jupiter.api.Test;
 import seedu.duke.command.AddModuleCommand;
 import seedu.duke.command.DeleteModuleCommand;
+import seedu.duke.command.ExitCommand;
 import seedu.duke.command.HelpCommand;
 import seedu.duke.command.IncompleteCommand;
 import seedu.duke.command.InvalidModuleCommand;
 import seedu.duke.command.SearchModuleCommand;
+import seedu.duke.command.SearchStringCommand;
 import seedu.duke.command.UnknownCommand;
 import seedu.duke.command.ViewTimetableCommand;
 
@@ -17,6 +19,8 @@ public class ParserTest {
     public void parse_searchInput_returnNewSearchCommand() {
         assertTrue(Parser.parse("search cs2113") instanceof SearchModuleCommand);
         assertTrue(Parser.parse("search Cs2040c") instanceof SearchModuleCommand);
+        assertTrue(Parser.parse("search Cs") instanceof SearchModuleCommand);
+        assertTrue(Parser.parse("search software engineering") instanceof SearchStringCommand);
     }
 
     @Test
@@ -24,7 +28,8 @@ public class ParserTest {
         assertTrue(Parser.parse("search") instanceof IncompleteCommand);
         assertTrue(Parser.parse("search csss2113") instanceof InvalidModuleCommand);
         assertTrue(Parser.parse("search Cs20401c") instanceof InvalidModuleCommand);
-        assertTrue(Parser.parse("search Cs2040c my love") instanceof UnknownCommand);
+        assertTrue(Parser.parse("search Cs2113 software engineering") instanceof UnknownCommand);
+        assertTrue(Parser.parse("search software engineering CS2113") instanceof UnknownCommand);
     }
 
     @Test
@@ -53,7 +58,13 @@ public class ParserTest {
     }
 
     @Test
+    public void parse_exitInput_returnNewExitCommand() {
+        assertTrue(Parser.parse("bye") instanceof ExitCommand);
+    }
+
+    @Test
     public void parse_unknownInput_returnNewUnknownCommand() {
+        assertTrue(Parser.parse("") instanceof UnknownCommand);
         assertTrue(Parser.parse("I love cs2113") instanceof UnknownCommand);
     }
 }
