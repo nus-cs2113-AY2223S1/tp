530f4110ce8054ab1b70775a4d61d67ff81147a5
shengiv <66622728+shengiv@users.noreply.github.com>
2022-10-10 13:15:51 +0800

    Add user interface test

--- src/main/java/seedu/duke/PassengerParser.java
+++ src/main/java/seedu/duke/PassengerParser.java
@@ -6,7 +6,8 @@ public class PassengerParser {
     private static PassengerList passengers = new PassengerList();
 
     public static void parse(String[] inputWords) {
-        if (inputWords[1].equalsIgnoreCase("add")) {
+        String command = inputWords[1];
+        if (command.equalsIgnoreCase("add")) {
             String[] passengerDetailsArray = Arrays.copyOfRange(inputWords, 2, inputWords.length);
             String passengerDetails = String.join(" ", passengerDetailsArray);
             passengers.addPassenger(passengerDetails);
--- src/main/java/seedu/duke/Ui.java
+++ src/main/java/seedu/duke/Ui.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 
 public class Ui {
     private Scanner in;
-    private static final String LOGO = "   _____ _             _____            _             _ \n"
+    private static final String LOGO = "   _____ _             _____            _             _\n"
             + "  / ____| |           / ____|          | |           | |\n"
             + " | (___ | | ___   _  | |     ___  _ __ | |_ _ __ ___ | |\n"
             + "  \\___ \\| |/ / | | | | |    / _ \\| '_ \\| __| '__/ _ \\| |\n"
             + "  ____) |   <| |_| | | |___| (_) | | | | |_| | | (_) | |\n"
             + " |_____/|_|\\_\\\\__, |  \\_____\\___/|_| |_|\\__|_|  \\___/|_|\n"
-            + "               __/ |                                    \n"
-            + "              |___/                                     \n";
+            + "               __/ |\n"
+            + "              |___/\n";
 
     public Ui() {
         in = new Scanner(System.in);
@@ -27,7 +27,7 @@ public class Ui {
 
     public static void showAddedPassenger(Passenger passenger) {
         System.out.println("Passenger " + passenger.getName() + " of "
-                + passenger.getFlightNumber() + " " + passenger.getSeatNumber() + " has been added. \n"
+                + passenger.getFlightNumber() + " " + passenger.getSeatNumber() + " has been added.\n"
         );
     }
 }
