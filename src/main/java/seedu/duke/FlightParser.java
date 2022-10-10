a9448a42bac5bf5c988532428b5e337d060d4dd8
LAPTOP-KFC5FK3E\kwuac <kwuachunren@gmail.com>
2022-10-10 20:17:21 +0800

    Fix minor parsing bug between Parser and FlightParser class

--- src/main/java/seedu/duke/FlightParser.java
+++ src/main/java/seedu/duke/FlightParser.java
@@ -3,17 +3,18 @@ package seedu.duke;
 import java.util.Arrays;
 
 public class FlightParser {
-    public static void parse(String[] inputWords) {
+    public static void parse(String lineInput) {
+        String[] inputWords = lineInput.split(" ");
         String command = inputWords[1];
         switch (command) {
         case "add":
-            FlightManager.addFlight(command);
+            FlightManager.addFlight(lineInput);
             break;
         case "list":
             FlightManager.printFlights();
             break;
         case "delete":
-            FlightManager.deleteFlight(command);
+            FlightManager.deleteFlight(lineInput);
             break;
         default:
             // temporary sout, change to exception handling
