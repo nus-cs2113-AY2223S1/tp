2405e1482413df164f4d0fe0e76d1785b54249eb
shengiv <66622728+shengiv@users.noreply.github.com>
2022-10-10 12:01:56 +0800

    Change Duke to SkyControl

---
+++ src/main/java/seedu/duke/SkyControl.java
@@ -0,0 +1,28 @@
+package seedu.duke;
+
+public class SkyControl {
+    private Ui ui;
+    private PassengerList passengers;
+
+    /**
+     * Main entry-point for the java.duke.Duke application.
+     */
+    public SkyControl() {
+        ui = new Ui();
+        passengers = new PassengerList();
+    }
+
+    public void run() {
+        ui.showWelcomeMessage();
+        String lineInput = ui.nextLineInput();
+        while (Parser.isRunning(lineInput)) {
+            Parser.parse(lineInput);
+            lineInput = ui.nextLineInput();
+        }
+    }
+
+    public static void main(String[] args) {
+        new SkyControl().run();
+    }
+}
+
