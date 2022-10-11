044bd7edafc58a6b290fa48bad2c168b5f1c63d5
joshuan98 <89023959+joshuan98@users.noreply.github.com>
2022-10-11 16:17:13 +0800

    Update Module class to reference University

--- src/main/java/seedu/duke/module/Module.java
+++ src/main/java/seedu/duke/module/Module.java
@@ -1,18 +1,24 @@
 package seedu.duke.module;
 
+import seedu.duke.university.University;
+
 public class Module {
     private String code;
     private String title;
     private String credit;
+    private University university;
 
-    public Module(String code, String title, String credit) {
+    public Module(String code, String title, String credit, University university) {
         assert code.length() > 0 : "Code cannot be empty";
         assert title.length() > 0 : "Title cannot be empty";
         assert credit.length() > 0 : "Credit cannot be empty";
+        assert university.getName().length() > 0 : "University name cannot be empty";
+        assert university.getCountry().length() > 0 : "University country cannot be empty";
 
         setCode(code);
         setTitle(title);
         setCredit(credit);
+        setUniversity(university);
     }
 
     @Override
@@ -42,5 +48,13 @@ public class Module {
 
     public void setCredit(String credit) {
         this.credit = credit;
+    }
+
+    public University getUniversity() {
+        return university;
+    }
+
+    public void setUniversity(University university) {
+        this.university = university;
     }
 }
