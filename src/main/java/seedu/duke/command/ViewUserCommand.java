e84a909263d4031211f8876b2972852c9622f385
Chien Jing Wei <jingweichien@gmail.com>
2022-10-11 22:20:47 +0800

    viewUserCmd done

--- src/main/java/seedu/duke/command/ViewUserCommand.java
+++ src/main/java/seedu/duke/command/ViewUserCommand.java
@@ -1,25 +1,50 @@
 package seedu.duke.command;
 
+import seedu.duke.Ui;
 import seedu.duke.exception.InsufficientArgumentsException;
+import seedu.duke.exception.InvalidArgumentException;
 import seedu.duke.exception.UserNotFoundException;
+import seedu.duke.parser.CommandParser;
 import seedu.duke.user.User;
 import seedu.duke.user.UserList;
 
 public class ViewUserCommand extends Command {
-    private final String[] args;
+    private final String[] parts;
     private final UserList userList;
 
-    public ViewUserCommand(String[] args, UserList userList) throws InsufficientArgumentsException {
-        this.args = args;
+    public ViewUserCommand(String[] parts, UserList userList) throws InsufficientArgumentsException {
+        this.parts = parts;
         this.userList = userList;
-        if (args.length != 1) {
+        if (parts.length != 1) {
             throw new InsufficientArgumentsException();
         }
     }
 
-    public boolean executeCommand() throws UserNotFoundException {
-        User user = this.userList.getUserById(args[DEFAULT_FIRST_INDEX]);
-        System.out.println(user);
+    public String getArgsViewUserCmd() throws InvalidArgumentException {
+        String arg;
+        if (parts[0].startsWith("u")) {
+            arg = CommandParser.getArgValue(parts[0]);
+        } else {
+            throw new InvalidArgumentException("Please input user name in the correct format");
+        }
+        return arg;
+    }
+
+    private boolean isValidUser(String userId) throws UserNotFoundException {
+        try {
+            userList.getUserById(userId);
+            return true;
+        } catch (UserNotFoundException e) {
+            throw new UserNotFoundException(e.getMessage());
+        }
+    }
+
+    public boolean executeCommand() throws UserNotFoundException, InvalidArgumentException {
+        String userName = getArgsViewUserCmd();
+        if (isValidUser(userName)) {
+            User user = this.userList.getUserById(userName);
+            Ui.viewUserMessage(user);
+        }
         return false;
     }
 }
