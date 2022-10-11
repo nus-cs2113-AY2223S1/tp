f8c81954577e67855b3822e89ed18f5c7a08c5a3
ngdeqi <deqi.angel@gmail.com>
2022-10-11 18:29:38 +0800

    Make PairingList addPairing compatible with Storage, add assertions and edit documentation

--- src/main/java/seedu/duke/PairingList.java
+++ src/main/java/seedu/duke/PairingList.java
@@ -7,65 +7,86 @@ import java.util.HashMap;
  */
 public class PairingList {
     private static final String SEPARATOR = " | ";
+    private static final String CURRENCY = "SGD";
+    private static final String OPEN_BRACKET = "[";
+    private static final String CLOSE_BRACKET = "]";
 
     private static final HashMap<String, String> clientPropertyPairs = new HashMap<>();
 
+    /**
+     * Default Constructor.
+     */
     public PairingList() {
 
     }
 
     /**
-     * Records which property a client is renting.
+     * Records which property a client is renting, with Client and Property objects as parameters.
      * @param client Client renting the property.
      * @param property Property being rented.
      */
     public void addPairing(Client client, Property property) {
         String clientPairingData = convertToPairingData(client);
         String propertyPairingData = convertToPairingData(property);
+        addPairing(clientPairingData, propertyPairingData);
+    }
 
+    /**
+     * Records which property a client is renting, with the client and property in the appropriate pairing formats as
+     * parameters. This is used to load the pairings from the pairing data file into the class variable.
+     * @param clientPairingData Pairing data of client that is renting the property.
+     * @param propertyPairingData Pairing data of property that is being rented.
+     */
+    public void addPairing(String clientPairingData, String propertyPairingData) {
+        assert !clientPropertyPairs.containsKey(clientPairingData) : "CommandPair: client is already renting property";
         clientPropertyPairs.put(clientPairingData, propertyPairingData);
     }
 
     /**
-     * Removes a client-property pair to indicate that the client is no longer renting that property.
+     * Deletes client-property pair to indicate that the client is no longer renting that property.
      * @param client Client who is no longer renting the property.
-     * @param property Property that is no loner being rented.
+     * @param property Property that is no longer being rented.
      */
     public void deletePairing(Client client, Property property) {
-
         String clientPairingData = convertToPairingData(client);
         String propertyPairingData = convertToPairingData(property);
+        assert clientPropertyPairs.containsKey(clientPairingData) : "CommandUnpair: client is not renting property";
 
         clientPropertyPairs.remove(clientPairingData, propertyPairingData);
     }
 
+    /**
+     * Returns true if the client is paired with a property.
+     * @param client Client whose pairing status is being checked.
+     * @return True if the client is currently paired with a property. False if not paired with a property
+     */
     public boolean isClientPairedWithProperty(Client client) {
         String clientPairingData = convertToPairingData(client);
         return clientPropertyPairs.containsKey(clientPairingData);
     }
 
     /**
-     * Overloaded method to convert client pairing data to a suitable string format.
+     * Converts client pairing data to a suitable string format.
      * @param client Client whose data is to be converted.
-     * @return Pairing data in a suitable string format.
+     * @return Client pairing data in a suitable string format.
      */
-    private String convertToPairingData(Client client) {
-        return client.getClientName() + SEPARATOR + client.getClientContactNumber();
+    public String convertToPairingData(Client client) {
+        return OPEN_BRACKET + client.getClientName()
+                + SEPARATOR + client.getClientContactNumber()
+                + CLOSE_BRACKET;
     }
 
     /**
-     * Overloaded method to convert property pairing data to a suitable string format.
+     * Converts property pairing data to a suitable string format.
      * @param property Property whose data is to be converted.
-     * @return Pairing data in a suitable string format.
+     * @return Property pairing data in a suitable string format.
      */
-    private String convertToPairingData(Property property) {
-        return property.getLandlordName()
-                + SEPARATOR
-                + property.getPropertyAddress()
-                + SEPARATOR
-                + property.getRentingPrice()
-                + SEPARATOR
-                + property.getUnitType();
+    public String convertToPairingData(Property property) {
+        return OPEN_BRACKET + property.getLandlordName()
+                + SEPARATOR + property.getPropertyAddress()
+                + SEPARATOR + CURRENCY + property.getRentingPrice()
+                + SEPARATOR + property.getUnitType()
+                + CLOSE_BRACKET;
     }
 
 }
