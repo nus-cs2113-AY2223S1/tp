5e49963b10c17673103a27f1b64e361da63278e9
qian <e0775289@u.nus.edu>
2022-10-11 17:30:56 +0800

    Updated view command for checkstyleTest

--- src/test/java/recipeditor/command/CommandResultTest.java
+++ src/test/java/recipeditor/command/CommandResultTest.java
@@ -1,6 +1,10 @@
 package recipeditor.command;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
 
 class CommandResultTest {
 
