500431911693dd3f242a8b74999e1ceb5932b529
LAPTOP-FOVQQ54S\80ly <junhaoliu2468@gmail.com>
2022-10-12 01:12:34 +0800

    checkstyle fix

--- src/main/java/seedu/duke/command/RemoveExerciseCommand.java
+++ src/main/java/seedu/duke/command/RemoveExerciseCommand.java
@@ -8,7 +8,7 @@ import seedu.duke.exercise.Exercise;
 import seedu.duke.exercise.ExerciseList;
 import seedu.duke.food.FoodList;
 
-public class RemoveExerciseCommand extends Command{
+public class RemoveExerciseCommand extends Command {
 
     private Ui ui;
     private String arguments;
@@ -16,11 +16,13 @@ public class RemoveExerciseCommand extends Command{
     private ExerciseList exerciseList;
 
 
-    public RemoveExerciseCommand(String arguments) { this.arguments = arguments; }
+    public RemoveExerciseCommand(String arguments) {
+        this.arguments = arguments;
+    }
 
-    private void removeExercise(String[] argumentList){
-        try{
-            if (argumentList.length != 2){
+    private void removeExercise(String[] argumentList) {
+        try {
+            if (argumentList.length != 2) {
                 throw new IllegalValueException("INVALID_NUMBER_INPUT");
             }
             int index = Integer.parseInt(argumentList[1]);
@@ -29,13 +31,14 @@ public class RemoveExerciseCommand extends Command{
         } catch (IllegalValueException e) {
             ui.output(e.getMessage());
         }
-
     }
+
     @Override
     public void execute() {
         String[] argumentList = Parser.getArgumentList(arguments);
         removeExercise(argumentList);
     }
+
     @Override
     public void setData(Ui ui, Biometrics biometrics, ExerciseList exerciseList, FoodList foodlist) {
         this.ui = ui;
