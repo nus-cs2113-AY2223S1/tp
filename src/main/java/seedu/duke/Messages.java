8f34943542edbce6ef030ef19ecb635d09253767
FeliciaBeatrice <feliciabeatrice7@gmail.com>
2022-10-11 22:25:29 +0800

    Fix Delete Client Exception Messages

--- src/main/java/seedu/duke/Messages.java
+++ src/main/java/seedu/duke/Messages.java
@@ -42,7 +42,9 @@ public class Messages {
             + "For client: add -client\n"
             + "For property: add -property";
 
-    public static final String MESSAGE_EMPTY_DELETE_DESCRIPTION = "OOPS!!! The description for delete cannot be empty.";
+    public static final String MESSAGE_EMPTY_DELETE_DESCRIPTION = "OOPS!!! Please use this format to delete:\n"
+            + "Client: delete -client ic/CLIENT_INDEX\n"
+            + "Property: delete -property ip/PROPERTY_INDEX";
 
     public static final String MESSAGE_INVALID_CLIENT_INDEX = "OOPS!!! Please enter a valid client index.";
 
@@ -50,8 +52,8 @@ public class Messages {
 
     public static final String MESSAGE_MISSING_CLIENT_INDEX_FLAG = "OOPS!!! Please provide the client index flag.";
 
-    public static final String MESSAGE_INVALID_CLIENT_INDEX_FLAG_FORMAT = "OOPS!!! Please use this format:\n"
-            + "delete -client ic/CLIENT_INDEX";
+    public static final String MESSAGE_INVALID_CLIENT_INDEX_FLAG_FORMAT = "OOPS!!! Please use this flag format:\n"
+            + "ic/CLIENT_INDEX";
 
     public static final String MESSAGE_PAIRED = "Pairing the following client and property: ";
 
