c973329f5eed0655ad06e4b347ac9eb7036c8871
Nay <naychi.w_naing@u.yale-nus.edu.sg>
2022-10-11 16:18:18 +0800

    Create AddCommandTest.java

---
+++ src/test/java/seedu/duke/command/AddCommandTest.java
@@ -0,0 +1,49 @@
+package seedu.duke.command;
+
+import org.junit.jupiter.api.Test;
+import seedu.duke.Parser;
+import seedu.duke.exception.IllegalValueException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+class AddCommandTest {
+
+    @Test
+    public void addCommand_invalidFoodDescription_throwsException() {
+        String command = "add food";
+        int calories = 100;
+        String fullCommand;
+        final String[] invalidFoodNames = { "", " ", "[]\\[;]" };
+        for (String food_name : invalidFoodNames) {
+            fullCommand = String.format("%s /%s /%d",
+                    command, food_name, calories);
+            Command c = Parser.parse(fullCommand);
+            try {
+                c.execute();
+                fail();
+            } catch (IllegalValueException e) {
+                assertEquals("Please provide valid food description inputs!", e.getMessage());
+            }
+        }
+    }
+
+
+    @Test
+    void execute_negativeCalories_exceptionThrown() {
+        String command = "add food";
+        String food_name = "chicken rice";
+        int calories = -112;
+        String fullCommand = String.format("%s /%s /%d",
+                command, food_name, calories);
+        Command c = Parser.parse(fullCommand);
+        try {
+            c.execute();
+            fail();
+        } catch (IllegalValueException e) {
+            assertEquals("Calories inputs need to be positive integer values!", e.getMessage());
+        }
+
+    }
+
+}
