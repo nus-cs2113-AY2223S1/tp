d722c77efabbaa7e7f1192cb5eaede4e43b6201c
Brominne <e0773671@u.nus.edu>
2022-10-11 11:06:33 +0800

    Standardize atrributes in Invalid Command

--- src/main/java/recipeditor/command/InvalidCommand.java
+++ src/main/java/recipeditor/command/InvalidCommand.java
@@ -3,13 +3,13 @@ package recipeditor.command;
 import recipeditor.ui.Ui;
 
 public class InvalidCommand extends Command {
-    public static final String TYPE = "invalid";
+    public static final String COMMAND_TYPE = "invalid";
     public static String message;
 
     public CommandResult execute() {
         //TODO: Execution of command
         // This is only the dummy code for testing
-        message = TYPE + "\n" + Ui.COMMAND;
+        message = COMMAND_TYPE + "\n" + Ui.COMMAND;
         return new CommandResult(message);
     }
 
