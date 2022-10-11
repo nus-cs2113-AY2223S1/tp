dedd8d4843454cade1da4e31b3a27f992048b87b
helpdesk1234 <ongzhihong2000@gmail.com>
2022-10-11 11:00:23 +0800

    Fix checkstyle issues

--- src/main/java/seedu/duke/parser/CommandParser.java
+++ src/main/java/seedu/duke/parser/CommandParser.java
@@ -12,43 +12,43 @@ public class CommandParser {
     public static Command getUserCommand(String userInput) throws InvalidUserCommandException {
         String[] userInputTokenized = userInput.split(" +");
         if (userInputTokenized.length < 1) {
-            throw new InvalidUserCommandException("Error! Missing command. " +
-                    "Please follow the command format provided!");
+            throw new InvalidUserCommandException("Error! Missing command. "
+                    + "Please follow the command format provided!");
         }
         String userInputCommand = userInputTokenized[0];
         switch (userInputCommand) {
         case "/create":
             if (!isValidCreateCommand(userInputTokenized)) {
-                throw new InvalidUserCommandException("Error! Invalid create command. " +
-                        "Please follow the command format provided");
+                throw new InvalidUserCommandException("Error! Invalid create command. "
+                        + "Please follow the command format provided");
             }
             CreateCommand newCreateCommand = new CreateCommand(userInputTokenized, CommandType.CREATE);
             return newCreateCommand;
         case "/add":
             if (!isValidAddCommand(userInputTokenized)) {
-                throw new InvalidUserCommandException("Error! Invalid add command. " +
-                        "Please follow the command format provided");
+                throw new InvalidUserCommandException("Error! Invalid add command. "
+                        + "Please follow the command format provided");
             }
             AddCommand newAddCommand = new AddCommand(userInputTokenized, CommandType.ADD);
             return newAddCommand;
         case "/view":
             if (!isValidViewCommand(userInputTokenized)) {
-                throw new InvalidUserCommandException("Error! Invalid view command. " +
-                        "Please follow the command format provided");
+                throw new InvalidUserCommandException("Error! Invalid view command. "
+                        + "Please follow the command format provided");
             }
             ViewCommand newViewCommand = new ViewCommand(userInputTokenized, CommandType.VIEW);
             return newViewCommand;
         case "/delete":
             if (!isValidDeleteCommand(userInputTokenized)) {
-                throw new InvalidUserCommandException("Error! Invalid delete command. " +
-                        "Please follow the command format provided");
+                throw new InvalidUserCommandException("Error! Invalid delete command. "
+                        + "Please follow the command format provided");
             }
             boolean isDeleteModule = userInputTokenized.length == 3;
             DeleteCommand newDeleteCommand = new DeleteCommand(userInputTokenized, CommandType.DELETE, isDeleteModule);
             return newDeleteCommand;
         default:
-            throw new InvalidUserCommandException("Error! Unidentified command. " +
-                    "Please follow the command format provided!");
+            throw new InvalidUserCommandException("Error! Unidentified command. "
+                    + "Please follow the command format provided!");
         }
     }
 
@@ -70,7 +70,7 @@ public class CommandParser {
     }
 
     private static boolean isValidDeleteCommand(String[] parameters) {
-        if (!parameters[1].startsWith("u/") || parameters.length > 3){
+        if (!parameters[1].startsWith("u/") || parameters.length > 3) {
             return false;
         } else if (parameters.length == 3 && !parameters[2].startsWith("m/")) {
             return false;
