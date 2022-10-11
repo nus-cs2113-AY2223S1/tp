fe6f772fc6caeebddeafd714ed88972611f5070e
markusteimann <markusteimann13@gmail.com>
2022-10-12 00:44:40 +0800

    Can now test our Wallet methods

--- src/test/java/seedu/duke/DukeTest.java
+++ src/test/java/seedu/duke/DukeTest.java
@@ -1,12 +1,61 @@
 package seedu.duke;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 class DukeTest {
+    Duke duke = new Duke();
+    Wallet emptyWallet = new Wallet("EmptyWalletUser" , "EmptyWalletPass");
+    Wallet filledWallet = new Wallet("FilledWalletUser" , "FilledWalletPass", 1000);
+
+
     @Test
-    public void sampleTest() {
-        assertTrue(true);
+    public void testEmptyWalletBalance() {
+        assertEquals(0, emptyWallet.getBalance());
     }
+
+    @Test
+    public void testEmptyWalletUserName() {
+        assertEquals("EmptyWalletUser", emptyWallet.getUserName());
+    }
+
+    @Test
+    public void testEmptyWalletPassWord() {
+        assertEquals("EmptyWalletPass", emptyWallet.getPassWord());
+    }
+
+    @Test
+    public void testFilledWalletBalance() {
+        assertEquals(1000, filledWallet.getBalance());
+    }
+
+    @Test
+    public void testFilledWalletUserName() {
+        assertEquals("FilledWalletUser", filledWallet.getUserName());
+    }
+
+    @Test
+    public void testFilledWalletPassWord() {
+        assertEquals("FilledWalletPass", filledWallet.getPassWord());
+    }
+
+    @Test
+    public void setWalletUserName() {
+        emptyWallet.setUserName("ChangedUserName");
+        assertEquals("ChangedUserName", emptyWallet.getUserName());
+    }
+
+    @Test
+    public void setWalletPassName() {
+        emptyWallet.setPassWord("ChangedPassWord");
+        assertEquals("ChangedPassWord", emptyWallet.getPassWord());
+    }
+
+
 }
