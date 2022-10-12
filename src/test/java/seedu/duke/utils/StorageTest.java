db267e455a84b44bc83f9fa5e253fb96f75dd2df
Koh Ngiap Hin <64647635+kohnh@users.noreply.github.com>
2022-10-13 01:33:21 +0800

    Modify Junit test

--- src/test/java/seedu/duke/utils/StorageTest.java
+++ src/test/java/seedu/duke/utils/StorageTest.java
@@ -1,7 +1,6 @@
 package seedu.duke.utils;
 
 import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Test;
 import seedu.duke.model.LessonType;
 import seedu.duke.model.Module;
 import seedu.duke.model.SelectedModule;
@@ -15,7 +14,7 @@ import java.util.Scanner;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StorageTest {
-    @RepeatedTest(5)
+    @RepeatedTest(value = 5, name = RepeatedTest.LONG_DISPLAY_NAME)
     public void saveState_state_outputFile() throws IOException {
         State state = new State();
         int semester = 1;
@@ -51,26 +50,25 @@ public class StorageTest {
         assertEquals(expectedString.toString(), actualString.toString());
     }
 
-    @RepeatedTest(5)
+    @RepeatedTest(value = 5, name = RepeatedTest.LONG_DISPLAY_NAME)
     public void openPreviousState_file_state() {
         Ui ui = new Ui();
         Storage storage = new Storage();
         State actualState = new State();
         storage.openPreviousState(actualState, ui);
-
+        assertEquals(1, actualState.getSemester());
         List<SelectedModule> selectedModules = actualState.getSelectedModulesList();
         SelectedModule selectedModule1 = selectedModules.get(0);
         Module module1 = selectedModule1.getModule();
-        Map<LessonType, String> selectedSlots1 = selectedModule1.getSelectedSlots();
-        SelectedModule selectedModule2 = selectedModules.get(1);
-        Module module2 = selectedModule2.getModule();
-        Map<LessonType, String> selectedSlots2 = selectedModule2.getSelectedSlots();
-
-        assertEquals(1, actualState.getSemester());
         assertEquals("CS1010", module1.moduleCode);
+        Map<LessonType, String> selectedSlots1 = selectedModule1.getSelectedSlots();
         assertEquals("01", selectedSlots1.get(LessonType.TUTORIAL));
         assertEquals("1", selectedSlots1.get(LessonType.SECTIONAL_TEACHING));
+
+        SelectedModule selectedModule2 = selectedModules.get(1);
+        Module module2 = selectedModule2.getModule();
         assertEquals("CS2113", module2.moduleCode);
+        Map<LessonType, String> selectedSlots2 = selectedModule2.getSelectedSlots();
         assertEquals("4", selectedSlots2.get(LessonType.TUTORIAL));
         assertEquals("1", selectedSlots2.get(LessonType.LECTURE));
     }
