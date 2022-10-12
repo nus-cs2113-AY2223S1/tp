78c5685c48826de91debda503e7f1904c781cbf0
insafhere <insaf@u.nus.edu>
2022-10-12 16:29:00 +0800

    Added JUnit Test

--- src/main/java/seedu/duke/ModuleList.java
+++ src/main/java/seedu/duke/ModuleList.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 
 public class ModuleList {
     public static ArrayList<Module> modules = new ArrayList<>();
+    public static int viewCount;
+    public static int mcsCount;
 
     public static void add(Module mod) {
         modules.add(mod);
@@ -23,21 +25,43 @@ public class ModuleList {
     }
     public static void view(String semester) {
         ArrayList<Module> matchingModules = new ArrayList<Module>();
+        viewCount = 0;
         for (Module mod : modules) {
             if (mod.semesterTaken.matches(semester)) {
                 matchingModules.add(mod);
+                viewCount++;  // increments the count for this particular semester (For JUnit Purpose)
             }
         }
         UI.listMessage(matchingModules, semester);
     }
     public static void mc(String semester) {
         ArrayList<Module> matchingModules = new ArrayList<Module>();
-        int mcCounter = 0;
+        mcsCount = 0;
         for (Module mod : modules) {
             if (mod.semesterTaken.matches(semester)) {
-                mcCounter += mod.mcs;
+                System.out.println("mcsCount from screeeen?? " + mcsCount);
+                System.out.println("mod.mcs from screeeen?? " + mod.mcs);
+                mcsCount =  mcsCount + mod.mcs;
             }
         }
-        UI.mcMessage(semester, mcCounter);
+        System.out.println("mcsCount from screeeen " + mcsCount);
+        UI.mcMessage(semester, mcsCount);
     }
+
+    // Returns total number of Modules in modules Array
+    public int getCount() {
+        return modules.size();
+    }
+
+    // Returns total number of Modules for the recent View command executed
+    public int getViewCount() {
+        return viewCount;
+    }
+
+    // Returns total number of MCs for the recent mcs command executed
+    public int getMcsCount() {
+        return mcsCount;
+    }
+
+
 }
