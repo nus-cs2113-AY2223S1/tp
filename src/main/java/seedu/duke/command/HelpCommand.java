792444026def0a26c279cbe1419990a54cf380ec
Koh Ngiap Hin <64647635+kohnh@users.noreply.github.com>
2022-10-12 12:58:24 +0800

    Fix checkstyle

--- src/main/java/seedu/duke/command/HelpCommand.java
+++ src/main/java/seedu/duke/command/HelpCommand.java
@@ -6,6 +6,7 @@ import seedu.duke.utils.Ui;
 
 public class HelpCommand extends Command {
     public static final String COMMAND_WORD = "help";
+
     public HelpCommand(String[] input) {
         super(input);
     }
