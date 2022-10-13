47546e63aa4ce887cac0e3a1cf277c53a485e062
ZHEQI SHEN <>
2022-10-13 20:23:02 +0800

    no message

---
+++ src/main/java/seedu/duke/changePetStatusCommand.java
@@ -0,0 +1,19 @@
+package seedu.duke;
+
+public class changePetStatusCommand {
+    private PetList Pets;
+    private boolean isHealthy;
+    private int index;
+
+    public changePetStatusCommand(PetList Pets, boolean isHealthy, int index){
+        this.Pets = Pets;
+        this.isHealthy = isHealthy;
+        this.index = index;
+    }
+
+    public void excecute() {
+        int changeIndex = index - 1;
+        Pet currPet = Pets.petList.get(changeIndex);
+        currPet.changeStatus(isHealthy);
+    }
+}
