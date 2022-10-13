ff25de67d6c82a387458806c5249935aaa4714ed
xzynos <lim_fang_jie@hotmail.com>
2022-10-13 22:54:36 +0800

    Add LocalLogger.java

---
+++ src/main/java/seedu/moneygowhere/logger/LocalLogger.java
@@ -0,0 +1,42 @@
+package seedu.moneygowhere.logger;
+
+import seedu.moneygowhere.MoneyGoWhere;
+import seedu.moneygowhere.common.Configurations;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@SuppressWarnings("FieldMayBeFinal")
+public class LocalLogger {
+    private Logger logger;
+
+    public LocalLogger() throws IOException {
+        Path localLoggerLoggingDirectory = Paths.get(Configurations.LOCAL_LOGGER_LOGGING_DIRECTORY);
+        if (!Files.exists(localLoggerLoggingDirectory)) {
+            Files.createDirectories(localLoggerLoggingDirectory);
+        }
+        Path localLoggerLoggingFilePath = Paths.get(Configurations.LOCAL_LOGGER_LOGGING_FILE_PATH);
+        if (!Files.exists(localLoggerLoggingFilePath)) {
+            Files.createFile(localLoggerLoggingFilePath);
+        }
+
+        FileHandler fileHandler = new FileHandler(
+                Configurations.LOCAL_LOGGER_LOGGING_FILE_PATH,
+                Configurations.LOCAL_LOGGER_LOGGING_FILE_TO_APPEND
+        );
+        fileHandler.setFormatter(new LocalLoggerFormatter());
+
+        logger = Logger.getLogger(MoneyGoWhere.class.getName());
+        logger.setUseParentHandlers(false);
+        logger.addHandler(fileHandler);
+    }
+
+    public void logInfo(String message) {
+        logger.log(Level.INFO, message);
+    }
+}
