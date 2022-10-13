d370b3f7230a2f223c00014f12a5db72ba5ef821
Amethyst <e0978391@u.nus.edu>
2022-10-14 03:40:09 +0800

    new Ui work, codes separated, some bugs fixed

--- src/main/java/seedu/duke/Account.java
+++ src/main/java/seedu/duke/Account.java
@@ -2,32 +2,40 @@ package seedu.duke;
 
 import java.util.Scanner;
 
+import seedu.duke.FinanceException.ExceptionCollection;
+
 public class Account {
     public static void accountCommands(Wallet wallet) {
-        Ui.accountEntry();
-        Scanner scan = new Scanner(System.in);
-        String in = scan.nextLine().toLowerCase();
-        while (!in.equals("bye")) {
+        boolean isAccountExit = false;
+        Ui.showAccountEntryMessage(wallet.getUserName());
+        while (!isAccountExit) {
+            String in = InputManager.receiveInputLine().toLowerCase();
             String[] splitInput = in.split(" ");
             String command = splitInput[0];
-            switch (command) {
-            case "balance":
-                System.out.println("Your current balance is: " + wallet.getBalance());
-                break;
-            case "deposit":
-                //Ui.deposit;
-                break;
-            case "withdraw":
-                //Ui.withdraw;
-                break;
-            case "delete":
-                //delete account
-                break;
-            default:
-                //throw COMMAND_NOT_KNOWN;
-                System.out.println("Unknown command");
+            try{
+                switch (command) {
+                case "balance":
+                    System.out.println("Your current balance is: " + wallet.getBalance());
+                    break;
+                case "deposit":
+                    //Ui.deposit;
+                    break;
+                case "withdraw":
+                    //Ui.withdraw;
+                    break;
+                case "delete":
+                    //delete account
+                    break;
+                case "exit":
+                    isAccountExit = true;
+                    Ui.showAccountExitMessage(wallet.getUserName());
+                    break;
+                default:
+                    throw new FinanceException(ExceptionCollection.COMMAND_TYPE_EXCEPTION);
+                }
+            } catch (FinanceException e) {
+                e.handleException();
             }
-            in = scan.nextLine().toLowerCase();
         }
     }
 }
