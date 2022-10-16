feb54c8427f01edfb29ccc16f3382a77c9f05a3b
Franky4566 <rachelfong6@gmail.com>
2022-10-16 16:34:06 +0800

    Update PassengerList assertion

--- src/main/java/seedu/duke/operationlist/PassengerList.java
+++ src/main/java/seedu/duke/operationlist/PassengerList.java
@@ -80,6 +80,8 @@ public class PassengerList extends OperationList {
     }
 
     private void validatePassenger(int index) {
+        getNumberOfPassengers();
+        assert index < numOfPassengers;
         isNamePresent = passengers.get(index).getName().contains(name);
         isFlightNumberPresent = passengers.get(index).getFlightNumber().contains(flightNumber);
         isSeatNumberPresent = passengers.get(index).getSeatNumber().contains(seatNumber);
@@ -101,6 +103,8 @@ public class PassengerList extends OperationList {
     }
 
     private void iteratePassengerDetail(int index) {
+        getNumberOfPassengers();
+        assert index < numOfPassengers;
         name = passengers.get(index).getName();
         departureDate = passengers.get(index).getDepartureDate();
         departureTime = passengers.get(index).getDepartureTime();
