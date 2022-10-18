6353025661d0faef5cd073c1601ea9c9547474a5
naz019 <nazbm823@gmail.com>
2022-10-19 01:49:56 +0800

    added sorting of review list by title and rating

---
+++ src/main/java/seedu/duke/SortCommand.java
@@ -0,0 +1,39 @@
+package seedu.duke;
+
+import java.util.Comparator;
+
+public class SortCommand extends Commands {
+    String[] userInput;
+
+    public SortCommand(ReviewList reviews, String[] userInput) {
+        super(reviews);
+        this.userInput = userInput;
+    }
+
+    @Override
+    public String execute() {
+        String output = "";
+
+        switch (userInput[1]) {
+        case "rating":
+            this.reviewList.inputs.sort(Comparator.comparing(Media::getRating));
+            break;
+
+        case "title":
+            this.reviewList.inputs.sort(Comparator.comparing(Media::getTitle));
+            break;
+
+        case "date":
+            break;
+
+        default:
+            break;
+        }
+
+        output += "Your list has been sorted by " + userInput[1] + '\n';
+        Commands listExecutor = new ListCommand(this.reviewList);
+        output = listExecutor.execute();
+
+        return output;
+    }
+}
