e3235ea43a05e7b3ca3dd89912504539da46b961
Matthew Phua <matthew.phua@icloud.com>
2022-10-19 17:12:14 +0800

    adding seperated categories for list command

--- src/main/java/seedu/duke/ListCommand.java
+++ src/main/java/seedu/duke/ListCommand.java
@@ -1,6 +1,9 @@
 package seedu.duke;
 
 public class ListCommand extends Commands {
+    private final String outputString = "---Here are the reviews in your list---\n";
+    private final String movieString = "\nMovies:\n";
+    private final String tvShowString = "\nTV Shows:\n";
 
     public ListCommand(ReviewList reviews) {
         super(reviews);
@@ -8,10 +11,22 @@ public class ListCommand extends Commands {
 
     @Override
     public String execute() {
-        String output = "Here are the reviews in your list:\n";
+        String moviesList = "";
+        String tvShowList = "";
+        int movieIndex = 1;
+        int tvShowIndex = 1;
+
         for (int i = 0; i < reviewList.inputs.size(); i++) {
-            output += (i + 1) + ". " + reviewList.inputs.get(i).toString() + "\n";
+            Media media = reviewList.inputs.get(i);
+            if (media instanceof Movie) {
+                moviesList += (movieIndex) + ". " + reviewList.inputs.get(i).toString() + "\n";
+                movieIndex += 1;
+            } else if (media instanceof TvShow) {
+                tvShowList += (tvShowIndex) + ". " + reviewList.inputs.get(i).toString() + "\n";
+                tvShowIndex += 1;
+            }
         }
+        String output = outputString + movieString + moviesList + tvShowString + tvShowList;
         output = output.strip();
         return output;
     }
