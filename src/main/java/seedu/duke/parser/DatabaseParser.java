b9b7b84e22c22b56e24ea172e739ed9e7d83d84a
joshuan98 <89023959+joshuan98@users.noreply.github.com>
2022-10-19 20:35:57 +0800

    Add javadoc comments and remove magic number

--- src/main/java/seedu/duke/parser/DatabaseParser.java
+++ src/main/java/seedu/duke/parser/DatabaseParser.java
@@ -8,12 +8,22 @@ import seedu.duke.exceptions.InvalidDatabaseLineException;
 public class DatabaseParser {
     private static Logger logger = Logger.getLogger("DatabaseParser");
 
+    private static final int PARTS_LENGTH = 15;
+
+    /**
+     * Parses current database line to obtain useful data and ignore useless data
+     * 
+     * @param line Current database line
+     * @return Array of strings containing useful data
+     * @throws InvalidDatabaseLineException If database line does not have the
+     *                                      intended number of parts
+     */
     public static String[] parseDatabaseLine(String line) throws InvalidDatabaseLineException {
         logger.log(Level.FINE, "Start parsing database line");
 
         String[] parts = line.split(",");
 
-        if (parts.length != 15) {
+        if (parts.length != PARTS_LENGTH) {
             throw new InvalidDatabaseLineException("Invalid database line: " + line);
         }
 
