a45b5ce6ba4b6bf445ea51402c3c219608598739
Koh Ngiap Hin <64647635+kohnh@users.noreply.github.com>
2022-10-20 16:19:18 +0800

    Include parser for ST1 and ST2

--- src/main/java/seedu/duke/parser/Parser.java
+++ src/main/java/seedu/duke/parser/Parser.java
@@ -18,7 +18,17 @@ import java.util.TreeMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * Handles the analysing of user input into logical syntactical components that is understood by the application.
+ */
 public class Parser {
+    /**
+     * Analyzes based on the first word which is supposed to be a keyword specifying the
+     * feature that the user wants to call upon.
+     *
+     * @param userInput The whole <code>String</code> that the user entered.
+     * @return The corresponding <code>Command</code>.
+     */
     public static Command parse(String userInput) {
         String[] keywords = userInput.split("\\s+");
         switch (keywords[0]) {
@@ -43,32 +53,34 @@ public class Parser {
         }
     }
 
+    /**
+     * Checks to see if the user supplied moduleCode is part of a valid module code. More information
+     * about a valid module code format can be seen in {@link InvalidModuleCommand}.
+     *
+     * @param moduleCode The user supplied moduleCode.
+     * @return If the user supplied a valid partial module code.
+     */
     public static boolean isPartialModuleCode(String moduleCode) {
         Pattern pattern = Pattern.compile("[a-z]{0,3}\\d{0,4}[a-z]?", Pattern.CASE_INSENSITIVE);
         Matcher matcher = pattern.matcher(moduleCode);
         return (matcher.find() && matcher.group().length() == moduleCode.length());
     }
 
+    /**
+     * Checks to see if the user supplied moduleCode is a valid module code.More information about
+     * a valid module code format can be seen in {@link InvalidModuleCommand}.
+     *
+     * @param moduleCode The user supplied moduleCode.
+     * @return If the user supplied a valid module code.
+     */
     public static boolean isModuleCode(String moduleCode) {
         Pattern pattern = Pattern.compile("[a-z]{2,3}\\d{4}[a-z]?", Pattern.CASE_INSENSITIVE);
         Matcher matcher = pattern.matcher(moduleCode);
         return (matcher.find() && matcher.group().length() == moduleCode.length());
     }
 
-    public static boolean isPartialModuleFaculty(String moduleFaculty) {
-        Pattern pattern = Pattern.compile("[a-z]{2,3}\\d{4}[a-z]?", Pattern.CASE_INSENSITIVE); // logic update needed
-        Matcher matcher = pattern.matcher(moduleFaculty);
-        return (matcher.find() && matcher.group().length() == moduleFaculty.length());
-    }
-
-    public static boolean isModuleFaculty(String moduleFaculty) {
-        Pattern pattern = Pattern.compile("[a-z]{2,3}\\d{4}[a-z]?", Pattern.CASE_INSENSITIVE); // logic update needed
-        Matcher matcher = pattern.matcher(moduleFaculty);
-        return (matcher.find() && matcher.group().length() == moduleFaculty.length());
-    }
-
-    public static boolean isMultiWordsCommand(String[] keywords) {
-        return keywords.length > 2;
+    public static boolean isValidModuleCode(String moduleCode) {
+        return isModuleCode(moduleCode) || isPartialModuleCode(moduleCode);
     }
 
     public static boolean isTwoWordsCommand(String[] keywords) {
@@ -79,30 +91,8 @@ public class Parser {
         return keywords.length == 1;
     }
 
-    public static boolean isValidModuleCode(String moduleCode) {
-        return isModuleCode(moduleCode) || isPartialModuleCode(moduleCode);
-    }
-
-    public static boolean isValidModuleFaculty(String moduleFaculty) {
-        return isModuleFaculty(moduleFaculty) || isPartialModuleFaculty(moduleFaculty);
-    }
-
-    private static boolean containsValidModuleCode(String[] keywords) {
-        for (int i = 1; i < keywords.length; i++) {
-            if (isValidModuleCode(keywords[i])) {
-                return true;
-            }
-        }
-        return false;
-    }
-
     private static boolean isValidTwoWordCommand(String[] keywords) {
         return isTwoWordsCommand(keywords) && isValidModuleCode(keywords[1]);
-    }
-
-    private static boolean isValidSemester(String[] keywords) {
-        int semesterInput = Integer.parseInt(keywords[1]);
-        return semesterInput > 0 && semesterInput <= 4;
     }
 
     public static Command searchCommand(String userInput) {
@@ -122,7 +112,6 @@ public class Parser {
         if (isValidTwoWordCommand(keywords)) {
             return command;
         } else {
-            // System.out.println("Invalid module code");
             return determineWrongCommand(keywords);
         }
     }
@@ -146,11 +135,40 @@ public class Parser {
     }
 
     public static Command selectSemesterCommand(String[] keywords, Command command) {
-        if (isValidSemester(keywords)) {
+        if (isValidSemester(keywords) || isValidSpecialTerm(keywords)) {
             return command;
         } else {
             return new UnknownCommand(keywords);
         }
+    }
+
+    private static boolean isValidSemester(String[] keywords) {
+        int semesterInput = Integer.parseInt(keywords[1]);
+        return semesterInput > 0 && semesterInput <= 4;
+    }
+
+    /**
+     * Checks for valid special term 1 and 2. Spaces and capitalization does not matter.
+     * If the user entered a valid special term, the keyword at index 1 will be replaced
+     * with the system understood semester number for special term (3/4) for the
+     * {@link #selectSemesterCommand(String[], Command)} to correctly instantiate
+     * the semester number.
+     *
+     * @param keywords Contains the user input split by spaces.
+     * @return If the user entered a valid special term.
+     */
+    private static boolean isValidSpecialTerm(String[] keywords) {
+        StringBuilder semester = new StringBuilder();
+        for (int i = 1; i < keywords.length; i++) {
+            semester.append(keywords[i]);
+        }
+        semester = new StringBuilder(semester.toString().toUpperCase());
+        if (semester.toString().matches("ST1|ST2|SPECIALTERM1|SPECIALTERM2")) {
+            int correctSemester = Integer.parseInt(semester.substring(semester.length() - 1)) + 2;
+            keywords[1] = String.valueOf(correctSemester);
+            return  true;
+        }
+        return false;
     }
 
     public static Map<String, String> parseParams(String description) {
