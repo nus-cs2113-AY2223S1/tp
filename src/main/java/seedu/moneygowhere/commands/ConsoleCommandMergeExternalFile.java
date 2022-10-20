cbe4839117366ada98c5c3ac60b0bf38e7cdebaf
Lok Qi Jun <e0544063@u.nus.edu>
2022-10-20 22:39:09 +0800

    Added command to merge external file

---
+++ src/main/java/seedu/moneygowhere/commands/ConsoleCommandMergeExternalFile.java
@@ -0,0 +1,16 @@
+package seedu.moneygowhere.commands;
+
+/**
+ * Stores the merge external file command and its arguments.
+ */
+public class ConsoleCommandMergeExternalFile extends ConsoleCommand{
+    private String filePath;
+
+    public ConsoleCommandMergeExternalFile(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+}
