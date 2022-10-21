e30f2a3189311efce6da3899bb6838c1bd47ade5
amitrahman1026 <amit.amit.rahman@gmail.com>
2022-10-22 03:33:50 +0800

    checkstyle and javadocs

--- src/main/java/seedu/duke/command/DisplaySelectedModuleListCommand.java
+++ src/main/java/seedu/duke/command/DisplaySelectedModuleListCommand.java
@@ -21,17 +21,17 @@ public class DisplaySelectedModuleListCommand extends Command {
             + "Usage:\t"
             + FORMAT;
 
-    public DisplaySelectedModuleListCommand (String[] input) {
+    public DisplaySelectedModuleListCommand(String[] input) {
         super(input);
         successful = false;
     }
 
     /**
      * Helper function to convert map of selected slots for each module to a list of key value pairs
-     * representing available lesson types of the module and the selected slot by the user
-     * @param map
+     * representing available lesson types of the module and the selected slot by the user.
+     * @param map of selected modules and their chosen slots
      */
-    public String convertWithStream (Map<LessonType, String> map) {
+    public String convertWithStream(Map<LessonType, String> map) {
         String mapAsString = map.keySet().stream()
                 .map(key -> key + "=" + map.get(key))
                 .collect(Collectors.joining("- ", "\t", "\n"));
@@ -39,11 +39,11 @@ public class DisplaySelectedModuleListCommand extends Command {
     }
 
     /**
-     * Helper function to format each selected module in a list to an organised format to display to users
-     * @param module
-     * @param selectedSlots
+     * Helper function to format each selected module in a list to an organised format to display to users.
+     * @param module currently being processed
+     * @param selectedSlots of module being processed
      */
-    public String formatPrintSelectedSlots (Module module, Map<LessonType, String> selectedSlots) {
+    public String formatPrintSelectedSlots(Module module, Map<LessonType, String> selectedSlots) {
         String formattedSelectedSlots;
 
         String moduleCode = module.moduleCode;
@@ -59,10 +59,10 @@ public class DisplaySelectedModuleListCommand extends Command {
     }
 
     /**
-     * Helper function to format list of selected modules to an organised format to display to users
-     * @param currentSelectedModules
+     * Helper function to format list of selected modules to an organised format to display to users.
+     * @param currentSelectedModules the list of all selectedModules and their selected slots for lessons
      */
-    public String formatPrintSelectedSlotsList (List<SelectedModule> currentSelectedModules) {
+    public String formatPrintSelectedSlotsList(List<SelectedModule> currentSelectedModules) {
         String formattedSelectedSlotsList = null;
 
         for (var selectedModule : currentSelectedModules) {
@@ -76,10 +76,10 @@ public class DisplaySelectedModuleListCommand extends Command {
     }
 
     /** This overridden execute method validates that the current list of selected module is not empty
-     * and lets users know whether the list command was successful or not
-     * @param state
-     * @param ui
-     * @param storage
+     * and lets users know whether the list command was successful or not.
+     * @param state current state of YAMOM
+     * @param ui utility class of YAMOM
+     * @param storage class that handles stores of Yamom
      */
     @Override
     public void execute(State state, Ui ui, Storage storage) {
@@ -97,15 +97,15 @@ public class DisplaySelectedModuleListCommand extends Command {
         ui.displayUi();
     }
 
-    /** This function does not exit the program
+    /** This function does not exit the program.
      */
     @Override
     public boolean isExit() {
         return false;
     }
 
-    /** output message indicates whether the list command was executed successfully
-     * @return outputMessage
+    /** This method indicates whether the list command was executed successfully.
+     * @return outputMessage indicates to user through ui display on status of selected modules list
      */
     @Override
     public String getExecutionMessage() {
