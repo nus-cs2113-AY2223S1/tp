5b35bc212d885ea66ffe4cfe69b261a388c61e6c
joshuan98 <89023959+joshuan98@users.noreply.github.com>
2022-10-22 16:47:12 +0800

    Add tests for invalid modules

--- src/test/java/seedu/duke/module/ModuleTest.java
+++ src/test/java/seedu/duke/module/ModuleTest.java
@@ -1,6 +1,7 @@
 package seedu.duke.module;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -14,5 +15,43 @@ public class ModuleTest {
         University university = new University("NUS", "Singapore");
         Module module = new Module("CS2113", "Software Engineering & Object-Oriented Programming", "4", university);
         assertEquals("CS2113 Software Engineering & Object-Oriented Programming 4MCs in NUS", module.toString());
+    }
+
+    @Test
+    void moduleConstructor_invalidModuleCode_throwsInvalidModuleException()
+            throws InvalidUniversityException {
+        University university = new University("NUS", "Singapore");
+
+        assertThrows(InvalidModuleException.class,
+                () -> new Module("", "Software Engineering & Object-Oriented Programming", "4", university));
+    }
+
+    @Test
+    void moduleConstructor_invalidModuleTitle_throwsInvalidModuleException()
+            throws InvalidUniversityException {
+        University university = new University("NUS", "Singapore");
+
+        assertThrows(InvalidModuleException.class,
+                () -> new Module("CS2113", "", "4", university));
+    }
+
+    @Test
+    void moduleConstructor_invalidModuleCredit_throwsInvalidModuleException()
+            throws InvalidUniversityException {
+        University university = new University("NUS", "Singapore");
+
+        assertThrows(InvalidModuleException.class,
+                () -> new Module("CS2113", "Software Engineering & Object-Oriented Programming", "", university));
+    }
+
+    @Test
+    void moduleConstructor_invalidUniversityName_throwsInvalidUniversityException() {
+        assertThrows(InvalidUniversityException.class,
+                () -> new University("", "Singapore"));
+    }
+
+    @Test
+    void moduleConstructor_invalidUniversityCountry_throwsInvalidUniversityException() {
+        assertThrows(InvalidUniversityException.class, () -> new University("NUS", ""));
     }
 }
