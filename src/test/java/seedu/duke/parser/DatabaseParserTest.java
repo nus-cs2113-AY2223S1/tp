1c7a81b9c0a33dc10e6a2c128e765e50c69e7069
joshuan98 <89023959+joshuan98@users.noreply.github.com>
2022-10-22 16:58:47 +0800

    Add tests for databaseParser

--- src/test/java/seedu/duke/parser/DatabaseParserTest.java
+++ src/test/java/seedu/duke/parser/DatabaseParserTest.java
@@ -1,5 +1,7 @@
 package seedu.duke.parser;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
@@ -7,6 +9,33 @@ import org.junit.jupiter.api.Test;
 import seedu.duke.exceptions.InvalidDatabaseLineException;
 
 public class DatabaseParserTest {
+    @Test
+    void parseDatabaseLine_validLine_correctLineData() throws InvalidDatabaseLineException {
+        String line = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o";
+
+        String[] lineData = DatabaseParser.parseDatabaseLine(line);
+        String[] expected = new String[] { "b", "c", "d", "e", "i", "j", "k" };
+
+        assertArrayEquals(expected, lineData);
+    }
+
+    @Test
+    void parseDatabaseLine_extraComma_correctLineData() throws InvalidDatabaseLineException {
+        String line = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,";
+        String[] lineData = DatabaseParser.parseDatabaseLine(line);
+
+        String[] expected = new String[] { "b", "c", "d", "e", "i", "j", "k" };
+
+        assertArrayEquals(expected, lineData);
+    }
+
+    @Test
+    void parseDatabaseLine_missingComma_expectException() {
+        String line = "a,b,c,d,e,f,g,h,i,j,k,l,m,n o";
+
+        assertThrows(InvalidDatabaseLineException.class, () -> DatabaseParser.parseDatabaseLine(line));
+    }
+
     @Test
     void parseDatabaseLine_emptyLine_expectException() {
         String line = "";
