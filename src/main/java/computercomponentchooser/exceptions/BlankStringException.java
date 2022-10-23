6534366dc32ebfe092a535a1c9e6a72100656bc2
P0tatoChips <69447339+P0tatoChips@users.noreply.github.com>
2022-10-23 17:21:05 +0800

    Added JavaDocs for BuildManager and Exception classes

--- src/main/java/computercomponentchooser/exceptions/BlankStringException.java
+++ src/main/java/computercomponentchooser/exceptions/BlankStringException.java
@@ -1,8 +1,16 @@
 package computercomponentchooser.exceptions;
 
+/**
+ * Represents the exception when the user input is blank.
+ */
 public class BlankStringException extends Exception {
     protected final String errorMessage = "String cannot be blank";
 
+    /**
+     * Returns the error message.
+     *
+     * @return The error message.
+     */
     public String getMessage() {
         return errorMessage;
     }
--- src/main/java/computercomponentchooser/exceptions/UnknownCommandException.java
+++ src/main/java/computercomponentchooser/exceptions/UnknownCommandException.java
@@ -4,10 +4,14 @@ package computercomponentchooser.exceptions;
  * An exception that occurs when the parse method in Parser class takes in a String as command that is
  * not part of any acceptable command.
  */
-
 public class UnknownCommandException extends Exception {
     protected final String errorMessage = "Please input a valid command";
 
+    /**
+     * Returns the error message.
+     *
+     * @return The error message.
+     */
     public String getMessage() {
         return errorMessage;
     }
