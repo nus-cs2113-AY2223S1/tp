7b9b0c9f19438c2cd19a1216cee054be83e3734c
DESKTOP-8022BOQ\Admin <lcsroy99@gmail.com>
2022-10-23 17:28:28 +0800

    Added JavaDocs to Cpu and minor changes to the methods in Cpu

--- src/main/java/computercomponentchooser/components/Cpu.java
+++ src/main/java/computercomponentchooser/components/Cpu.java
@@ -1,9 +1,29 @@
 package computercomponentchooser.components;
 
+/**
+ * Represents the CPU in the computer build.
+ */
 public class Cpu extends Component {
+
+    /**
+     * The socket of the CPU.
+     */
     protected String socket;
+
+    /**
+     * Clock speed of the CPU in GHz.
+     */
     protected String clock;
 
+    /**
+     * Initializes a new CPU.
+     *
+     * @param name the name of the CPU
+     * @param price the price of the CPU
+     * @param power the power consumption of the CPU
+     * @param socket the socket of the CPU
+     * @param clock the clock speed of the CPU
+     */
     public Cpu(String name, String price, String power, String socket, String clock) {
         this.name = name;
         this.price = price;
@@ -12,60 +32,79 @@ public class Cpu extends Component {
         this.clock = clock;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPrice() {
-        return price;
-    }
-
-    public void setPrice(String price) {
-        this.price = price;
-    }
-
+    /**
+     * Gets the power of the CPU.
+     *
+     * @return the power of the CPU
+     */
     public String getSocket() {
         return socket;
     }
 
+    /**
+     * Gets the clock speed of the CPU in GHz.
+     *
+     * @return the clock speed of the CPU in GHz
+     */
     public String getClock() {
         return clock;
     }
 
+    /**
+     * Sets the clock speed of the CPU in GHz.
+     *
+     * @param clock the clock speed of the CPU in GHz
+     */
     public void setClock(String clock) {
         this.clock = clock;
     }
 
-    public String getPower() {
-        return power;
-    }
-
-    public void setPower(String power) {
-        this.power = power;
-    }
-
+    /**
+     * Creates a string representation of all the information of the CPU.
+     * Uses this method to display the information of the CPU.
+     *
+     * @return a string representation of all the information of the CPU
+     */
     @Override
     public String toString() {
         return super.toString() + String.format(" [%s] [%s GHz]", socket, clock);
     }
 
+    /**
+     * Creates a string representation of all the information of the CPU.
+     * Uses this method to save the information of the CPU to a csv file.
+     *
+     * @return a string representation of all the information of the CPU
+     */
     public String toCsv() {
         return super.toCsv() + "," + socket + "," + clock;
     }
 
+    /**
+     * Creates a string representation of all the information of the CPU.
+     * Uses this method to save the information of the CPU to a text file.
+     *
+     * @return a string representation of all the information of the CPU
+     */
     @Override
     public String saveAsString() {
         return super.saveAsString() + "/" + socket + "/" + clock;
     }
 
+    /**
+     * Gets all the information of the CPU.
+     *
+     * @return all the information of the CPU
+     */
     public String getDetails() {
         return super.getDetails() + String.format("\nSocket: %s\nClock: %s GHz", socket, clock);
     }
 
+    /**
+     * Gets the type of the computer part.
+     *
+     * @return the type of the computer part
+     */
     public String getType() {
         return "cpu";
     }
