5364ec1868899646a6944ada5a19a0af0d9d1dab
helpdesk1234 <ongzhihong2000@gmail.com>
2022-10-23 13:42:10 +0800

    Add logic support for timetable and lessons in Duke

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -3,17 +3,14 @@ package seedu.duke;
 import java.util.ArrayList;
 import java.util.NoSuchElementException;
 
-import seedu.duke.command.Command;
-import seedu.duke.command.Database;
-import seedu.duke.command.DatabaseStorage;
-import seedu.duke.command.ListCommand;
-import seedu.duke.command.ViewCommand;
-import seedu.duke.command.FavouriteCommand;
+import seedu.duke.command.*;
+import seedu.duke.exceptions.InvalidModuleException;
 import seedu.duke.exceptions.InvalidUserCommandException;
 import seedu.duke.exceptions.ModuleNotFoundException;
 import seedu.duke.exceptions.UniversityNotFoundException;
 import seedu.duke.module.Module;
 import seedu.duke.parser.CommandParser;
+import seedu.duke.timetable.TimetableManager;
 import seedu.duke.ui.Ui;
 import seedu.duke.module.ModuleMapping;
 import seedu.duke.university.University;
@@ -34,6 +31,9 @@ public class Duke {
         System.out.print(Ui.printCommands());
         DatabaseStorage.loadDatabase();
         UserUniversityListManager userUniversityListManager = UserStorageParser.getSavedLists();
+        //To be changed to loading timetable from database
+        //Timetable Manager timetableManager = UserStorageParser.getSavedTimetables();
+        TimetableManager timetableManager = new TimetableManager();
 
         while (!shouldExit) {
             try {
@@ -48,20 +48,31 @@ public class Duke {
                     break;
                 case DELETE:
                     try {
-                        if (newUserCommand.getModuleCode() == null) {
-                            userUniversityListManager.deleteList(newUserCommand.getUniversityName());
+                        DeleteCommand deleteCommand = (DeleteCommand) newUserCommand;
+                        if (deleteCommand.getLesson() != null) {
+                            timetableManager.addLesson(deleteCommand.getLesson());
+                            //UserStorageParser.storeTimetable(timetableManager);
                         } else {
-                            userUniversityListManager.deleteModule(newUserCommand.getUniversityName(),
-                                    newUserCommand.getModuleCode());
+                            if (deleteCommand.getModuleCode() == null) {
+                                userUniversityListManager.deleteList(newUserCommand.getUniversityName());
+
+                            } else {
+                                userUniversityListManager.deleteModule(newUserCommand.getUniversityName(),
+                                        newUserCommand.getModuleCode());
+                                timetableManager.deleteTimetable(newUserCommand.getUniversityName());
+                                //UserStorageParser.storeTimetable(timetableManager);
+                            }
+                            UserStorageParser.storeCreatedLists(userUniversityListManager);
                         }
-                        UserStorageParser.storeCreatedLists(userUniversityListManager);
                     } catch (NoSuchElementException e) {
                         Ui.printExceptionMessage(e);
                     }
                     break;
                 case CREATE:
                     userUniversityListManager.createList(newUserCommand.getUniversityName());
+                    timetableManager.createTimetable(newUserCommand.getUniversityName());
                     UserStorageParser.storeCreatedLists(userUniversityListManager);
+                    //UserStorageParser.storeTimetable(timetableManager);
                     break;
                 case VIEW:
                     try {
@@ -79,15 +90,21 @@ public class Duke {
                     break;
                 case ADD:
                     try {
-                        ModuleMapping moduleMapping = Database.findPuMapping(newUserCommand.getModuleCode());
-                        Module puModule = moduleMapping.getPartnerUniversityModule();
-                        Module nusModule = moduleMapping.getNusModule();
-                        UserModuleMapping userModuleToAdd = new UserModuleMapping(puModule.getCode(),
-                                puModule.getTitle(), nusModule.getCode(), nusModule.getTitle(),
-                                nusModule.getCredit(), puModule.getCredit(), puModule.getUniversity().getName(),
-                                puModule.getUniversity().getCountry());
-                        userUniversityListManager.addModule(newUserCommand.getUniversityName(), userModuleToAdd);
-                        UserStorageParser.storeCreatedLists(userUniversityListManager);
+                        AddCommand addCommand = (AddCommand) newUserCommand;
+                        if (addCommand.getLesson() != null) {
+                            timetableManager.addLesson(addCommand.getLesson());
+                            //UserStorageParser.storeTimetable(timetableManager);
+                        } else {
+                            ModuleMapping moduleMapping = Database.findPuMapping(newUserCommand.getModuleCode());
+                            Module puModule = moduleMapping.getPartnerUniversityModule();
+                            Module nusModule = moduleMapping.getNusModule();
+                            UserModuleMapping userModuleToAdd = new UserModuleMapping(puModule.getCode(),
+                                    puModule.getTitle(), nusModule.getCode(), nusModule.getTitle(),
+                                    nusModule.getCredit(), puModule.getCredit(), puModule.getUniversity().getName(),
+                                    puModule.getUniversity().getCountry());
+                            userUniversityListManager.addModule(newUserCommand.getUniversityName(), userModuleToAdd);
+                            UserStorageParser.storeCreatedLists(userUniversityListManager);
+                        }
                     } catch (ModuleNotFoundException | NoSuchElementException e) {
                         Ui.printExceptionMessage(e);
                     }
@@ -136,7 +153,7 @@ public class Duke {
                 default:
                     break;
                 }
-            } catch (InvalidUserCommandException e) {
+            } catch (InvalidUserCommandException | InvalidModuleException | ModuleNotFoundException e) {
                 Ui.printExceptionMessage(e);
             }
         }
