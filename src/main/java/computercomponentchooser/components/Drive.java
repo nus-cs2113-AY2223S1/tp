d50b50c1dbbab1c87af16bb1bddf0fec3b395365
DESKTOP-8022BOQ\Admin <lcsroy99@gmail.com>
2022-10-23 17:28:51 +0800

    Added JavaDocs to Drive

--- src/main/java/computercomponentchooser/components/Drive.java
+++ src/main/java/computercomponentchooser/components/Drive.java
@@ -1,51 +1,119 @@
 package computercomponentchooser.components;
 
+/**
+ * Represents the drive in the computer build.
+ */
 public class Drive extends Component {
+
+    /**
+     * The size of the drive in GB.
+     */
     protected String size;
+
+    /**
+     * The type of the drive.
+     */
     protected String driveType;
 
-    public Drive(String name, String price, String power, String size, String type) {
+    /**
+     * Initializes a new drive.
+     *
+     * @param name the name of the drive
+     * @param price the price of the drive
+     * @param power the power consumption of the drive
+     * @param size the size of the drive
+     * @param driveType the type of the drive
+     */
+    public Drive(String name, String price, String power, String size, String driveType) {
         this.name = name;
         this.price = price;
         this.size = size;
-        this.driveType = type;
+        this.driveType = driveType;
         this.power = power;
     }
 
+    /**
+     * Gets the size of the drive.
+     *
+     * @return the size of the drive
+     */
     public String getSize() {
         return size;
     }
 
+    /**
+     * Sets the size of the drive.
+     *
+     * @param size the size of the drive
+     */
     public void setSize(String size) {
         this.size = size;
     }
 
+    /**
+     * Gets the type of the drive.
+     *
+     * @return the type of the drive
+     */
     public String getDriveType() {
         return driveType;
     }
 
+    /**
+     * Sets the type of the drive.
+     *
+     * @param driveType the type of the drive
+     */
     public void setDriveType(String driveType) {
         this.driveType = driveType;
     }
 
+    /**
+     * Creates a string representation of all the information of the drive.
+     * Uses this method to display the information of the drive.
+     *
+     * @return a string representation of all the information of the drive
+     */
     @Override
     public String toString() {
         return super.toString() + String.format(" [%s GB] [%s]", size, driveType);
     }
 
+    /**
+     * Creates a string representation of all the information of the drive.
+     * Uses this method to save the information of the drive to a csv file.
+     *
+     * @return a string representation of all the information of the drive
+     */
     public String toCsv() {
         return super.toCsv() + "," + size + "," + driveType;
     }
 
+    /**
+     * Creates a string representation of all the information of the drive.
+     * Uses this method to save the information of the drive to a text file.
+     *
+     * @return a string representation of all the information of the drive
+     */
     @Override
     public String saveAsString() {
         return super.saveAsString() + "/" + size + "/" + driveType;
     }
 
+    /**
+     * Gets all the information of the drive.
+     *
+     * @return all the information of the drive
+     */
     public String getDetails() {
         return super.getDetails() + String.format("\nSize: %s GB\nType: %s", size, driveType);
     }
 
+    /**
+     * Gets the type of the computer part.
+     *
+     * @return the type of the computer part
+     */
     public String getType() {
         return "drive";
     }
