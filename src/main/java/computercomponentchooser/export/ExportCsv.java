371c0d91bd3b9ad289317148f7cfbccf0a2b50d0
GitPancaked <99005378+GitPancaked@users.noreply.github.com>
2022-10-23 15:22:34 +0800

    Added JavaDocs to ExportText and ExportCSV

--- src/main/java/computercomponentchooser/export/ExportCsv.java
+++ src/main/java/computercomponentchooser/export/ExportCsv.java
@@ -9,16 +9,38 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.Files;
 
+/**
+ * Exports the builds to a CSV file.
+ */
 public class ExportCsv {
+
+    /**
+     * The file directory that the files are stored in.
+     */
     private static final String FILE_DIRECTORY = "data";
+
+    /**
+     * The file name of the CSV file that contains the builds.
+     */
     private static String EXPORT_TEXT_FILE_PATH = "data/exportAllBuildsCSV.csv";
 
     private final BuildManager buildManager;
 
+    /**
+     * Initializes a new ExportCsv object.
+     *
+     * @param buildManager The buildManager to be used.
+     */
     public ExportCsv(BuildManager buildManager) {
         this.buildManager = buildManager;
     }
 
+    /**
+     * Exports all the builds to a CSV file.
+     *
+     * @param buildManager The buildManager to be used.
+     * @throws IOException
+     */
     public static void exportAllBuildsCsv(BuildManager buildManager) throws IOException {
         Path fileDirectory = Paths.get(FILE_DIRECTORY);
         if (!Files.exists(fileDirectory)) {
