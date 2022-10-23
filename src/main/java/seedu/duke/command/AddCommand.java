39f992a3245fdc9b8689bf471f54d2f85998f651
helpdesk1234 <ongzhihong2000@gmail.com>
2022-10-23 14:13:42 +0800

    Update AddCommand to set lesson in Constructor

--- src/main/java/seedu/duke/command/AddCommand.java
+++ src/main/java/seedu/duke/command/AddCommand.java
@@ -4,8 +4,10 @@ import seedu.duke.timetable.Lesson;
 
 public class AddCommand extends Command {
     Lesson lesson;
+
     public AddCommand(String[] parameters, CommandType commandType, Lesson lesson) {
         super(parameters, commandType);
+        this.lesson = lesson;
         this.universityName = parameters[1].substring(2);
         this.moduleCode = parameters[2].substring(2);
     }
