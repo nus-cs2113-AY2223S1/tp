a82c1e2d9eaba28f13eb85f04f1a8d40c94cae2a
ZHEQI SHEN <>
2022-10-24 20:09:13 +0800

    change pet isHealthy status

---
+++ src/main/java/command/petCommand/ChangePetHealthStatus.java
@@ -0,0 +1,24 @@
+package command.petCommand;
+
+import pet.Pet;
+import pet.PetList;
+
+import java.util.Random;
+
+public class ChangePetHealthStatus {
+    private int index;
+    public boolean isHealthy;
+
+    public ChangePetHealthStatus(int index, boolean isHealthy){
+        this.index = index;
+        this.isHealthy = isHealthy;
+    }
+
+    public void excecute() {
+        Random random = new Random();
+        int randomNumber = random.nextInt(5) + 1;
+        int changeIndex = index - 1;
+        Pet currPet = PetList.petList.get(changeIndex);
+        currPet.isHealthy = isHealthy;
+    }
+}
