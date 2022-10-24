cc21cc7017be84fbee3672f003ef11269685dc56
chinhan99 <62207919+chinhan99@users.noreply.github.com>
2022-10-24 15:51:46 +0800

    Add exception handling to ignore printing of message in BudgetCommandTest

--- src/test/java/seedu/duke/command/BudgetCommandTest.java
+++ src/test/java/seedu/duke/command/BudgetCommandTest.java
@@ -6,6 +6,9 @@ import seedu.duke.Ui;
 import seedu.duke.data.Budget;
 import seedu.duke.data.TransactionList;
 import seedu.duke.exception.MoolahException;
+import seedu.duke.exception.StorageWriteErrorException;
+
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -13,23 +16,28 @@ public class BudgetCommandTest {
     //@@author wcwy
     @Test
     public void execute_setValidBudget_expectedSuccessfulBudgetSet() throws MoolahException {
-        TransactionList transactions = new TransactionList();
-        Ui ui = new Ui();
-        Storage storage = new Storage();
+        try {
+            TransactionList transactions = new TransactionList();
+            Ui ui = new Ui();
+            Storage storage = new Storage();
 
-        BudgetCommand budgetCommand = new BudgetCommand();
-        budgetCommand.execute(transactions, ui, storage);
-        assertEquals(Budget.getBudget(), 1000);
+            BudgetCommand budgetCommand = new BudgetCommand();
+            budgetCommand.execute(transactions, ui, storage);
+            assertEquals(Budget.getBudget(), 1000);
 
-        budgetCommand.setBudgetAmount(100000);
-        budgetCommand.execute(transactions, ui, storage);
-        assertEquals(Budget.getBudget(), 100000);
+            budgetCommand.setBudgetAmount(100000);
+            budgetCommand.execute(transactions, ui, storage);
+            assertEquals(Budget.getBudget(), 100000);
 
-        budgetCommand.setBudgetAmount(Long.MAX_VALUE);
-        budgetCommand.execute(transactions, ui, storage);
-        assertEquals(Budget.getBudget(), Long.MAX_VALUE);
+            budgetCommand.setBudgetAmount(Long.MAX_VALUE);
+            budgetCommand.execute(transactions, ui, storage);
+            assertEquals(Budget.getBudget(), Long.MAX_VALUE);
 
-        // Reset budget back
-        Budget.setBudget(1000);
+            // Reset budget back to default
+            budgetCommand.setBudgetAmount(1000);
+            budgetCommand.execute(transactions, ui, storage);
+        } catch (StorageWriteErrorException ignore) {
+            // Ignores exception to prevent printing of error message
+        }
     }
 }
