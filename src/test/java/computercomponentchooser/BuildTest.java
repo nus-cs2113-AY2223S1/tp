20c3caeb9f1fd580de494635ecc31e8f403453d8
P0tatoChips <69447339+P0tatoChips@users.noreply.github.com>
2022-10-24 18:50:38 +0800

    Added try catch in tests because of gradle tests

--- src/test/java/computercomponentchooser/BuildTest.java
+++ src/test/java/computercomponentchooser/BuildTest.java
@@ -3,6 +3,7 @@ package computercomponentchooser;
 import computercomponentchooser.components.Cpu;
 import computercomponentchooser.components.Gpu;
 import computercomponentchooser.components.Motherboard;
+import computercomponentchooser.exceptions.NegativeNumberException;
 import org.junit.jupiter.api.BeforeEach;
 
 public class BuildTest {
@@ -14,9 +15,13 @@ public class BuildTest {
 
     @BeforeEach
     public void setUp() {
-        cpu1 = new Cpu("cpu1", "123", "12", "1", "4.0");
-        gpu1 = new Gpu("gpu1", "456", "45", "4","5");
-        mobo1 = new Motherboard("mobo1", "123", "12", "1", "full ATX", "2", "2");
+        try {
+            cpu1 = new Cpu("cpu1", "123", "12", "1", "4.0");
+            gpu1 = new Gpu("gpu1", "456", "45", "4", "5");
+            mobo1 = new Motherboard("mobo1", "123", "12", "1", "full ATX", "2", "2");
+        } catch (NegativeNumberException | NumberFormatException e) {
+            System.out.println("Error in setUp");
+        }
         build1 = new Build("build1");
         build1.addComponent("cpu", cpu1);
         build1.addComponent("gpu", gpu1);
