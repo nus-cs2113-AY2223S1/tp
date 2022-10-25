dc597c0aa51ef4adee2bc15626894d6acbcdff19
Marvin Pranajaya <marvinpofficial@gmail.com>
2022-10-25 16:42:31 +0800

    Fix update command parser

--- src/main/java/seedu/commands/UpdateCommand.java
+++ src/main/java/seedu/commands/UpdateCommand.java
@@ -22,23 +22,20 @@ public class UpdateCommand extends Command {
      * @param api api that the programme has authenticated.
      * @return updated carpark list
      */
-    private CarparkList updateCarparkList(Api api) {
+    private String updateCarparkList(Api api) {
         try {
             //fetch api
             api.syncFetchData();
             //update json
-            CarparkList updatedCarparkList = new CarparkList(CommonFiles.LTA_FILE_PATH,
-                    CommonFiles.LTA_BACKUP_FILE_PATH);
-            return updatedCarparkList;
+            return "Update Successful.";
         } catch (ParkingException e) {
-            e.getMessage();
-            return null;
+            return "Update unsuccessful. " + e.getMessage();
         }
     }
 
     @Override
     public CommandResult execute() {
-        CarparkList result = updateCarparkList(api);
-        return new CommandResult(result.toString());
+        String message = updateCarparkList(api);
+        return new CommandResult(message);
     }
 }
