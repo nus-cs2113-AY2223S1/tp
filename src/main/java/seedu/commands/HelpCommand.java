d1952464a136817846cd10c8d25863ea279098d2
Marvin Pranajaya <marvinpofficial@gmail.com>
2022-10-26 01:06:14 +0800

    Add Help command

---
+++ src/main/java/seedu/commands/HelpCommand.java
@@ -0,0 +1,36 @@
+package seedu.commands;
+
+import static seedu.common.CommonData.AUTH_FORMAT;
+import static seedu.common.CommonData.EXIT_FORMAT;
+import static seedu.common.CommonData.FAVOURITE_FORMAT;
+import static seedu.common.CommonData.FIND_FORMAT;
+import static seedu.common.CommonData.HELP_FORMAT;
+import static seedu.common.CommonData.LIST_FORMAT;
+import static seedu.common.CommonData.SEARCH_FORMAT;
+import static seedu.common.CommonData.UNFAVOURITE_FORMAT;
+import static seedu.common.CommonData.UPDATE_FORMAT;
+
+/**
+ * Represents a command to display the possible commands in parKING.
+ */
+public class HelpCommand extends Command {
+    public static final String COMMAND_WORD = "help";
+
+    @Override
+    public CommandResult execute() {
+        String message = "Here are the list of available commands to use! "
+                + "Refer to the user guide at https://ay2223s1-cs2113-t17-4.github.io/tp/UserGuide.html for more "
+                + "information.\n";
+        message += HELP_FORMAT + "\n";
+        message += EXIT_FORMAT + "\n";
+        message += AUTH_FORMAT + "\n";
+        message += UPDATE_FORMAT + "\n";
+        message += LIST_FORMAT + "\n";
+        message += FIND_FORMAT + "\n";
+        message += SEARCH_FORMAT + "\n";
+        message += FAVOURITE_FORMAT + "\n";
+        message += UNFAVOURITE_FORMAT;
+
+        return new CommandResult(message);
+    }
+}
