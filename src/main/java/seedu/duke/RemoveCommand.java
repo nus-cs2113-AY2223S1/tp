2634fbb0ae07669e29e110769e626002ed470f4d
Matthew Phua <matthew.phua@icloud.com>
2022-10-25 17:32:41 +0800

    fixing delete command

--- src/main/java/seedu/duke/RemoveCommand.java
+++ src/main/java/seedu/duke/RemoveCommand.java
@@ -2,19 +2,32 @@ package seedu.duke;
 
 public class RemoveCommand extends Commands {
     int index;
+    Class type;
 
-    public RemoveCommand(ReviewList reviews, int index) {
+    public RemoveCommand(ReviewList reviews, Class type, int index) {
         super(reviews);
+        this.type = type;
         this.index = index;
     }
 
     @Override
     public String execute() {
-        assert reviewList.inputs.size() != 0 : "Cannot remove from an array of size 0";
-        Media deleteMedia = reviewList.inputs.get(index);
-        reviewList.remove(index);
-        return "Noted. I've deleted the following media:" + '\n' + '\t' + deleteMedia.toString()
-                + "\nNow you have " + reviewList.inputs.size() + " reviews in the list.";
+        assert reviewList.inputs.size() > 0 : "Cannot remove from an array of size 0";
+        int current = 0;
+        Media deleteMedia = null;
+        for (int i = 0; i < reviewList.inputs.size(); i++) {
+            Media currMedia = reviewList.inputs.get(i);
+            if (currMedia.getClass().equals(type)) {
+                current += 1;
+                if (current == index) {
+                    deleteMedia = reviewList.inputs.get(i);
+                    reviewList.remove(i);
+                    return "Noted. I've deleted the following media:" + '\n' + '\t' + deleteMedia.toString()
+                            + "\nNow you have " + reviewList.inputs.size() + " reviews in the list.";
+                }
+            }
+        }
+        return "Unable to find item for specified type and index";
     }
 
 }
