15634f26b4264fd2b6807140128a220140cf67c7
naz019 <nazbm823@gmail.com>
2022-10-26 02:22:42 +0800

    added tests for Sort Commands

---
+++ src/test/java/seedu/duke/SortCommandTest.java
@@ -0,0 +1,94 @@
+package seedu.duke;
+
+import org.junit.jupiter.api.Test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Comparator;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+class SortCommandTest {
+    @Test
+    void execute_sortByRating_lowestRatingFirst() {
+        ReviewList storedReviews = new ReviewList();
+        Movie movie1 = new Movie("Harry Potter",99, "SciFi","10-01-2020");
+        Movie movie2 = new Movie("Harry Potter",50, "SciFi","10-01-2020");
+        Movie movie3 = new Movie("Harry Potter",1, "SciFi","10-01-2020");
+
+        storedReviews.add(movie1);
+        storedReviews.add(movie2);
+        storedReviews.add(movie3);
+
+        storedReviews.inputs.sort(Comparator.comparing(Media::getRating));
+
+        Movie assertMovie = (Movie) storedReviews.inputs.get(0);
+
+        assertEquals(1.0,assertMovie.rating);
+    }
+
+    @Test
+    void execute_sortByTitle_earlierLetterFirst() {
+        ReviewList storedReviews = new ReviewList();
+        Movie movie1 = new Movie("Iron Man",99, "SciFi","10-01-2020");
+        Movie movie2 = new Movie("Avengers",99, "SciFi","10-01-2020");
+        Movie movie3 = new Movie("Black Panther",99, "SciFi","10-01-2020");
+
+        storedReviews.add(movie1);
+        storedReviews.add(movie2);
+        storedReviews.add(movie3);
+
+        storedReviews.inputs.sort(Comparator.comparing(Media::getTitle));
+
+        Movie assertMovie = (Movie) storedReviews.inputs.get(0);
+
+        assertEquals("Avengers",assertMovie.title);
+    }
+
+    @Test
+    void execute_sortByGenre_earlierLetterFirst() {
+        ReviewList storedReviews = new ReviewList();
+        Movie movie1 = new Movie("Avengers",99, "SciFi","10-01-2020");
+        Movie movie2 = new Movie("Avengers",99, "Action","10-01-2020");
+        Movie movie3 = new Movie("Avengers",99, "Romance","10-01-2020");
+
+        storedReviews.add(movie1);
+        storedReviews.add(movie2);
+        storedReviews.add(movie3);
+
+        storedReviews.inputs.sort(Comparator.comparing(Media::getGenre));
+
+        Movie assertMovie = (Movie) storedReviews.inputs.get(0);
+
+        assertEquals("Action",assertMovie.genre);
+    }
+
+    @Test
+    void execute_sortByDateWatched_earlierDateFirst() {
+        ReviewList storedReviews = new ReviewList();
+        Movie movie1 = new Movie("Avengers",99, "Action","01-01-2021");
+        Movie movie2 = new Movie("Avengers",99, "Action","15-02-2020");
+        Movie movie3 = new Movie("Avengers",99, "Action","31-03-2022");
+
+        storedReviews.add(movie1);
+        storedReviews.add(movie2);
+        storedReviews.add(movie3);
+
+        storedReviews.inputs.sort(Comparator.comparing(Media::getDateWatched));
+
+        Movie assertMovie = (Movie) storedReviews.inputs.get(0);
+
+        try {
+            DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy", Locale.ENGLISH);
+            Date date = dateFormat.parse("15-02-2020");
+            assertEquals(date, assertMovie.getDateWatched());
+        } catch (ParseException e) {
+            fail("Encountered ParseException.");
+        }
+    }
+}
