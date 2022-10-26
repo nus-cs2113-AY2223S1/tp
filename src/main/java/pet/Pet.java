a1327675932f246cbb0ea514fd3ec5606aad9ec1
nvknow <1375782557@qq.com>
2022-10-26 11:32:08 +0800

    initPetStatus when adding a pet

--- src/main/java/pet/Pet.java
+++ src/main/java/pet/Pet.java
@@ -1,4 +1,8 @@
 package pet;
+import pet.PetList.*;
+
+import java.util.Random;
+
 
 public class Pet {
     public String name;
@@ -6,6 +10,9 @@ public class Pet {
     // private int id;
     public Boolean isHealthy;
     public PetStatus status;
+
+    public static final int NUM_OF_STATUS = 5;
+
 
     public static int numOfPets = 0;
     //public static int idTracker = 1;
@@ -15,12 +22,56 @@ public class Pet {
         this.species = species;
         // this.id = id;
         this.isHealthy = isHealthy;
+        initPetStatus();
     }
 
     public String toString() {
-        String isHealthy = this.isHealthy ? "healthy" : "unhealthy";
+//        String isHealthy = this.isHealthy ? "healthy" : "unhealthy";
         return String.format("I am a cute %s, my name is %s, and I am currently %s",
-                species, name, isHealthy);
+                species, name, status);
+    }
+
+    private void initPetStatus() {
+        Random random = new Random();
+        int randomNumber = random.nextInt(NUM_OF_STATUS) + 1;
+        if (this.isHealthy){
+            switch (randomNumber){
+            case 1:
+                status = PetHealthyStatus.happy;
+                break;
+            case 2:
+                status = PetHealthyStatus.boring;
+                break;
+            case 3:
+                status = PetHealthyStatus.sleepy;
+                break;
+            case 4:
+                status = PetHealthyStatus.energetic;
+                break;
+            case 5:
+                status = PetHealthyStatus.crazy;
+                break;
+            }
+        }
+        else if (isHealthy = false){
+            switch (randomNumber){
+            case 1:
+                status = PetUnhealthyStatus.exhausted;
+                break;
+            case 2:
+                status = PetUnhealthyStatus.painful;
+                break;
+            case 3:
+                status = PetUnhealthyStatus.injured;
+                break;
+            case 4:
+                status = PetUnhealthyStatus.bleeding;
+                break;
+            case 5:
+                status = PetUnhealthyStatus.dying;
+                break;
+            }
+        }
     }
 }
 
