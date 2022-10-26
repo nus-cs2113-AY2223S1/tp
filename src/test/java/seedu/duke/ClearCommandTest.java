08a16d7a80a80d245f221fce1919a4f6427d789f
Matthew Phua <matthew.phua@icloud.com>
2022-10-26 18:39:01 +0800

    adding clear tests

--- src/test/java/seedu/duke/ClearCommandTest.java
+++ src/test/java/seedu/duke/ClearCommandTest.java
@@ -6,9 +6,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ClearCommandTest {
+    static final String confirmClear = "Your list is now cleared.";
+    ReviewList storedReviews = new ReviewList();
+
     @Test
-    public void sampleTest() {
-        assertTrue(true);
+    public void clearTest() {
+        ClearCommand command = new ClearCommand(storedReviews);
+        assertEquals(confirmClear, command.execute());
+        assertEquals((new ReviewList()).inputs, storedReviews.inputs);
     }
 
 }
