cce71579f4eab4382d293fb414b82ffebaef7fcd
nvknow <1375782557@qq.com>
2022-10-26 17:31:29 +0800

    delete an assertion to make it work

--- src/test/java/appointment/PetTest.java
+++ src/test/java/appointment/PetTest.java
@@ -11,6 +11,5 @@ public class PetTest {
         PetList petList = new PetList();
         Pet newPet = new Pet("YuHuan", "cat", true);
         petList.petList.add(newPet);
-//        assertEquals(newPet.isHealthy, true);
     }
 }
