7ca0e1d893b350fba33cabfc954faaf49f9b3a66
chinhan99 <62207919+chinhan99@users.noreply.github.com>
2022-10-26 19:47:59 +0800

    Update minimum transaction value

--- src/main/java/seedu/duke/common/Constants.java
+++ src/main/java/seedu/duke/common/Constants.java
@@ -19,7 +19,7 @@ public class Constants {
     public static int MAX_TRANSACTIONS_COUNT = 1000000;
 
     // The amount of one transaction is allowed to be in the range or 0 <= x <= 10000000
-    public static int MIN_AMOUNT_VALUE = 0;
+    public static int MIN_AMOUNT_VALUE = 1;
     public static int MAX_AMOUNT_VALUE = 10000000;
 
     // The amount of transaction is allowed to be in the range or 1 <= x <= MAX_AMOUNT * MAX_TRANSACTION
