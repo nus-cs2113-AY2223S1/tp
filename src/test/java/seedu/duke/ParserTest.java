f88bb9a480ac87b6b356e19640cff9202743bd83
INDR <indraneel.paranjape@gmail.com>
2022-10-27 14:02:01 +0800

    Checkstyle error fixed

--- src/test/java/seedu/duke/ParserTest.java
+++ src/test/java/seedu/duke/ParserTest.java
@@ -2,7 +2,7 @@ package seedu.duke;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class ParserTest {
     ReviewList rv = new ReviewList();
