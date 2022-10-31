65e25851a4540b6a2516f5eddaa1db699ee6190e
eehongchan <eehongchan@gmail.com>
2022-10-31 18:27:46 +0800

    Fix bug regarding command 'favourite list' not working after favourite file is changed manually

--- src/main/java/seedu/commands/FavouriteCommand.java
+++ src/main/java/seedu/commands/FavouriteCommand.java
@@ -1,11 +1,16 @@
 package seedu.commands;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 import seedu.data.Carpark;
 import seedu.data.CarparkList;
 import seedu.exception.DuplicateCarparkException;
 import seedu.exception.FileWriteException;
 import seedu.exception.NoCarparkFoundException;
+import seedu.exception.NoFileFoundException;
 import seedu.files.Favourite;
+import seedu.ui.Ui;
 
 /**
  * Represents the 'favourite' command to save carparks in a list.
@@ -17,6 +22,7 @@ public class FavouriteCommand extends Command {
     private static final String SEPARATOR_STRING = "===========================================";
     private final String argument;
     private final Favourite favourite;
+    private final Ui ui;
 
     /**
      * Constructor for FavouriteCommand
@@ -28,6 +34,7 @@ public class FavouriteCommand extends Command {
         this.argument = argument;
         this.favourite = favourite;
         this.carparkList = carparkList;
+        this.ui = new Ui();
     }
 
     /**
@@ -39,19 +46,31 @@ public class FavouriteCommand extends Command {
     public CommandResult execute() {
         try {
             if (argument.equalsIgnoreCase("list")) {
+                favourite.updateFavouriteList();
                 StringBuilder content = new StringBuilder();
+                ArrayList<String> validCarparkIDs = new ArrayList<>();
                 boolean isEmpty = true;
+                boolean isValid = true;
                 for (String id : Favourite.getFavouriteList()) {
                     if (id.isEmpty()) {
                         continue;
-                    }
-                    if (isEmpty) {
-                        isEmpty = false;
+                    } else if (carparkList.isCarparkValid(id)) {
+                        validCarparkIDs.add(id);
+                        if (isEmpty) {
+                            isEmpty = false;
+                        } else {
+                            content.append("\n" + SEPARATOR_STRING + "\n");
+                        }
+                        assert carparkList.findCarpark(id) != null : "Could not find carpark!";
+                        content.append(carparkList.findCarpark(id).toString());
                     } else {
-                        content.append("\n" + SEPARATOR_STRING + "\n");
+                        isValid = false;
                     }
-                    assert carparkList.findCarpark(id) != null : "Could not find carpark!";
-                    content.append(carparkList.findCarpark(id).toString());
+                }
+                if (!isValid) {
+                    favourite.replaceFavouriteList(validCarparkIDs);
+                    favourite.writeFavouriteList();
+                    ui.print("Some values were invalid. These were skipped.\n");
                 }
                 if (isEmpty) {
                     return new CommandResult("There is no favourites in the list!");
@@ -63,14 +82,17 @@ public class FavouriteCommand extends Command {
                 result.setFavourite(true);
                 return new CommandResult("Added Carpark " + argument + " to favourites!");
             }
-        } catch (NoCarparkFoundException e) {
-            return new CommandResult(e.getMessage());
-        } catch (DuplicateCarparkException f) {
-            return new CommandResult(f.getMessage());
-        } catch (FileWriteException e) {
+        } catch (NoCarparkFoundException noCarparkFoundException) {
+            return new CommandResult(noCarparkFoundException.getMessage());
+        } catch (DuplicateCarparkException duplicateCarparkException) {
+            return new CommandResult(duplicateCarparkException.getMessage());
+        } catch (FileWriteException fileWriteException) {
             return new CommandResult("Error in setting " + argument + " as favourite.");
+        } catch (IOException ioException) {
+            return new CommandResult(ioException.getMessage());
+        } catch (NoFileFoundException noFileFoundException) {
+            return new CommandResult(noFileFoundException.getMessage());
         }
-
     }
 
     /**
