e45e06944d5b555aaaff7253b74cf92a2473258b
kyrene chua <cwx.kyrene@gmail.com>
2022-11-01 08:06:16 +0800

    edited the HelpCommand and CommonData file for new filter commands

--- src/main/java/seedu/commands/HelpCommand.java
+++ src/main/java/seedu/commands/HelpCommand.java
@@ -3,7 +3,9 @@ package seedu.commands;
 import static seedu.common.CommonData.AUTH_FORMAT;
 import static seedu.common.CommonData.EXIT_FORMAT;
 import static seedu.common.CommonData.FAVOURITE_FORMAT;
+import static seedu.common.CommonData.FILTER_ADDRESS_FORMAT;
 import static seedu.common.CommonData.FILTER_FORMAT;
+import static seedu.common.CommonData.FILTER_ID_FORMAT;
 import static seedu.common.CommonData.FIND_FORMAT;
 import static seedu.common.CommonData.HELP_FORMAT;
 import static seedu.common.CommonData.LIST_FORMAT;
@@ -28,6 +30,8 @@ public class HelpCommand extends Command {
         message += AUTH_FORMAT + "\n";
         message += UPDATE_FORMAT + "\n";
         message += FILTER_FORMAT + "\n";
+        message += FILTER_ADDRESS_FORMAT + "\n";
+        message += FILTER_ID_FORMAT + "\n";
         message += FIND_FORMAT + "\n";
         message += FAVOURITE_FORMAT + "\n";
         message += UNFAVOURITE_FORMAT;
