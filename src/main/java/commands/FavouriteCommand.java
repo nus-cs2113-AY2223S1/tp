be2869b262c64921043a02c305726564aace1ded
naz019 <nazbm823@gmail.com>
2022-11-03 00:16:24 +0800

    updated favourite command to follow media type index and updated tests

--- src/main/java/commands/FavouriteCommand.java
+++ src/main/java/commands/FavouriteCommand.java
@@ -1,7 +1,13 @@
 package commands;
 
+import exceptions.DukeException;
 import seedu.duke.Media;
+import seedu.duke.Movie;
 import seedu.duke.ReviewList;
+import seedu.duke.TvShow;
+import seedu.duke.Ui;
+
+import java.util.logging.Level;
 
 public class FavouriteCommand extends Commands {
     String[] userInput;
@@ -13,33 +19,73 @@ public class FavouriteCommand extends Commands {
 
     @Override
     public String execute() {
-        String index = userInput[1];
-        int favouriteIndex;
         String output = "";
         Media media;
+        int current = 0;
 
         if (!userInput[1].equals("list")) {
-            favouriteIndex = Integer.parseInt(index) - 1;
-            media = this.reviewList.inputs.get(favouriteIndex);
+            try {
+                String index = userInput[2];
+                int favouriteIndex = Integer.parseInt(index);
 
-            if (media.isFavourite() == false) {
-                media.setFavourite(true);
-                output = "The following review has been starred:\n" + media.toString();
-            } else {
-                media.setFavourite(false);
-                output = "The following review has been unstarred:\n" + media.toString();
+                if (favouriteIndex <= 0 || favouriteIndex > reviewList.inputs.size()) {
+                    return "Unable to find item for specified type and index";
+                }
+
+                String typeString = userInput[1];
+                Class type;
+                if (typeString.equals("movie")) {
+                    type = Movie.class;
+                } else if (typeString.equals("tv")) {
+                    type = TvShow.class;
+                } else {
+                    throw new DukeException();
+                }
+
+                for (int i = 0; i < reviewList.inputs.size(); i++) {
+                    media = reviewList.inputs.get(i);
+                    if (media.getClass().equals(type)) {
+                        current += 1;
+                    }
+                    if (current == favouriteIndex) {
+                        if (!media.isFavourite()) {
+                            media.setFavourite(true);
+                            output = "The following review has been starred:\n" + media.toString();
+                        } else {
+                            media.setFavourite(false);
+                            output = "The following review has been unstarred:\n" + media.toString();
+                        }
+                        return output;
+                    }
+                }
+            } catch (DukeException | NumberFormatException e) {
+                Ui.print("Incomplete or wrongly formatted command, try again.");
             }
-        } else {
-            output = "Your favourites are:\n";
 
-            for (int i = 0; i < this.reviewList.inputs.size(); i++) {
-                media = this.reviewList.inputs.get(i);
-                if (media.isFavourite() == true) {
-                    output += media.toString() + '\n';
+            return "Unable to find item for specified type and index";
+        } else {
+            ReviewList favouritesList = new ReviewList();
+            int count = 0;
+
+            for (int i = 0; i < reviewList.inputs.size(); i++) {
+                media = reviewList.inputs.get(i);
+
+                if (media.isFavourite()) {
+                    favouritesList.add(media);
+                    count += 1;
                 }
             }
-        }
 
-        return output;
+            if (count == 1) {
+                output = "You have " + count + " favourite in total.\n";
+            } else {
+                output = "You have " + count + " favourites in total.\n";
+            }
+
+
+            Commands listHelper = new ListCommand(favouritesList);
+            output += listHelper.execute();
+            return output;
+        }
     }
 }
