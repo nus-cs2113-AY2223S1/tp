c110a22d11dadcb43d4ea106d515b898c6b78f70
insafhere <insaf@u.nus.edu>
2022-11-02 16:05:24 +0800

    modified help message

--- src/main/java/seedu/duke/commands/Help.java
+++ src/main/java/seedu/duke/commands/Help.java
@@ -7,13 +7,13 @@ public class Help extends Command {
 
     final String message = "## Command Summary\n" +
             "\n" +
-            "* Add module `add m/MODULE_CODE s/YEAR_NUMBER_SEMESTER_NUMBER mc/NUMBER_OF_MCS g/GRADE`\n" +
-            "* Deletes module `delete m/MODULE_CODE`\n" +
-            "* View modules `view s/YEAR_NUMBER_SEMESTER_NUMBER`\n" +
-            "* Calculate MCs `mcs s/YEAR_NUMBER_SEMESTER_NUMBER`\n" +
-            "* Finds module information `find <KEYWORD>`\n" +
-            "* Checks for program eligibility `check <PROGRAM>`\n" +
-            "* Exits the App `exit`";
+            "* Add a Module : add m/MODULE_CODE s/YEAR_NUMBER_SEMESTER_NUMBER mc/NUMBER_OF_MCS g/GRADE \n" +
+            "* Delete a module : delete m/MODULE_CODE \n" +
+            "* View modules : view s/YEAR_NUMBER_SEMESTER_NUMBER \n" +
+            "* Calculate MCs taken : mcs s/YEAR_NUMBER_SEMESTER_NUMBER \n" +
+            "* Finds module information : find <KEYWORD> \n" +
+            "* Checks for SEP/NOC program eligibility : check <PROGRAM> \n" +
+            "* Exits the App : exit ";
 
     public Help() {
     }
