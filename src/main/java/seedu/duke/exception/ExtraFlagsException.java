90b7e023a39ff55e2e22952b7ab17f81c2805aee
FeliciaBeatrice <feliciabeatrice7@gmail.com>
2022-11-02 10:12:45 +0800

    Add and handle extra flags exception

---
+++ src/main/java/seedu/duke/exception/ExtraFlagsException.java
@@ -0,0 +1,15 @@
+package seedu.duke.exception;
+
+public class ExtraFlagsException extends DukeParseException {
+
+    private final String message;
+
+    public ExtraFlagsException(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return message;
+    }
+}
