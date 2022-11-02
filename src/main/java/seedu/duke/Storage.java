7dadc76476fac9cd030a70d11c506ef9004568d6
chinhan99 <62207919+chinhan99@users.noreply.github.com>
2022-11-03 00:07:00 +0800

    Fix constant names in Storage class

--- src/main/java/seedu/duke/Storage.java
+++ src/main/java/seedu/duke/Storage.java
@@ -31,9 +31,17 @@ public class Storage {
     private static final String FILE_PATH = "data/duke.txt";
     private static final String DELIMITER = " \\| ";
     private static final int NUMBER_OF_STORED_PARAMETERS = 5;
-    private static final int TENS = 10;
-    private static final int HUNDREDS = 100;
-    private static final int THOUSANDS = 1000;
+    private static final int YEAR_TENTH = 10;
+    private static final int YEAR_HUNDREDTH = 100;
+    private static final int YEAR_THOUSANDTH = 1000;
+    private static final int MONTH_TENTH = 10;
+    private static final int DAY_TENTH = 10;
+    private static final String APPENDED_ZERO_TO_MONTH_BELOW_TEN = "0";
+    private static final String APPENDED_ZERO_TO_DAY_BELOW_TEN = "0";
+    private static final String APPENDED_ZERO_TO_YEAR_BELOW_THOUSAND = "0";
+    private static final String APPENDED_ZEROES_TO_YEAR_BELOW_HUNDRED = "00";
+    private static final String APPENDED_ZEROES_TO_YEAR_BELOW_TEN = "000";
+
 
     private TransactionList storedTransactions;
 
@@ -198,7 +206,9 @@ public class Storage {
 
 
     /**
-     * Synthesizes the date into dateString , which would be in the correct format to process.
+     * Synthesizes the date variables into dateString, which would be in the correct format for parsing.
+     * When extracting date variables like month from date, zeros in front of the variable would be removed by default.
+     * E.g. A month was stored as 9. A zero has to be added, so month would be equal to 09, and correct for parsing.
      *
      * @param date which would be processed into a string.
      * @return dateString, in the correct format to be parsed.
@@ -209,18 +219,19 @@ public class Storage {
         String dateOfMonth = String.valueOf(date.getDayOfMonth());
         String month = String.valueOf(date.getMonthValue());
         String year = String.valueOf(date.getYear());
-        if (date.getMonthValue() < TENS) {
-            month = "0" + month;
+        if (date.getMonthValue() < MONTH_TENTH) {
+            // If month value is less than 10, a zero has to be added to the front of the variable for parsing reasons
+            month = APPENDED_ZERO_TO_MONTH_BELOW_TEN + month;
         }
-        if (date.getDayOfMonth() < TENS) {
-            dateOfMonth = "0" + dateOfMonth;
+        if (date.getDayOfMonth() < DAY_TENTH) {
+            dateOfMonth = APPENDED_ZERO_TO_DAY_BELOW_TEN + dateOfMonth;
         }
-        if (date.getYear() < TENS) {
-            year = "000" + year;
-        } else if (date.getYear() < HUNDREDS) {
-            year = "00" + year;
-        } else if (date.getYear() < THOUSANDS) {
-            year = "0" + year;
+        if (date.getYear() < YEAR_TENTH) {
+            year = APPENDED_ZEROES_TO_YEAR_BELOW_TEN + year;
+        } else if (date.getYear() < YEAR_HUNDREDTH) {
+            year = APPENDED_ZEROES_TO_YEAR_BELOW_HUNDRED + year;
+        } else if (date.getYear() < YEAR_THOUSANDTH) {
+            year = APPENDED_ZERO_TO_YEAR_BELOW_THOUSAND + year;
         }
 
         String dateString = dateOfMonth + month + year;
