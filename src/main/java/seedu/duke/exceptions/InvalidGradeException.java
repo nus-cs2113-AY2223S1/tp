772e2cdc95a867b943047af0cf3ede0aea9303ca
Zhai-Yuxin <e0772744@u.nus.edu>
2022-11-02 22:01:40 +0800

    edit to check grade

--- src/main/java/seedu/duke/exceptions/InvalidGradeException.java
+++ src/main/java/seedu/duke/exceptions/InvalidGradeException.java
@@ -5,7 +5,7 @@ public class InvalidGradeException extends Exception {
         return !grade.matches("[0-9]+");
     }
     public static boolean checkValidGrade(String grade) {
-        String[] possibleGrades = {"A", "A+","A-","B","B+","B-", "C", "C+","D", "D+", "F", "F*", "S", "U"};
+        String[] possibleGrades = {"A", "A+","A-","B","B+","B-", "C", "C+","D", "D+", "F", "F*", "S", "U", "-"};
         for (String possibleGrade : possibleGrades) {
             if (grade.equals(possibleGrade)) {
                 return true;
