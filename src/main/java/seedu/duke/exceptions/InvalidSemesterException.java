0aa928abe562a293a1339a8951f0d42cc83edd33
Ishita <ishitamandal06@gmail.com>
2022-11-03 17:33:19 +0800

    changes to valid semester

--- src/main/java/seedu/duke/exceptions/InvalidSemesterException.java
+++ src/main/java/seedu/duke/exceptions/InvalidSemesterException.java
@@ -3,20 +3,26 @@ package seedu.duke.exceptions;
 public class InvalidSemesterException extends Exception {
 
     public static boolean invalidFormat(String semester) {
-        if (semester.contains("Y")) {
-            if (semester.contains("S")) {
-                if (semester.indexOf("Y") < semester.indexOf("S")) {
-                    return false;
+        if( semester.length() == 4) {
+            if (semester.contains("Y")) {
+                if (semester.contains("S")) {
+                    if (semester.indexOf("Y") < semester.indexOf("S")) {
+                        return false;
+                    }
                 }
             }
         }
+
         return true;
     }
 
     public static boolean invalidYearNumber(String semester) {
         String year = semester.substring(semester.indexOf('Y') + 1,semester.indexOf('S'));
         if (year.length() == 1 && year.matches("[0-9]+")) {
-            return false;
+            if (Integer.parseInt(year) < 7 && Integer.parseInt(year) > 0) {
+                return false;
+            }
+
         }
         return true;
     }
@@ -24,7 +30,7 @@ public class InvalidSemesterException extends Exception {
     public static boolean invalidSemesterNumber(String semester) {
         String year = semester.substring(semester.indexOf('S') + 1);
         if (year.length() == 1 && year.matches("[0-9]+")) {
-            if (Integer.parseInt(year) < 4) {
+            if (Integer.parseInt(year) > 0 && Integer.parseInt(year) < 3 ) {
                 return false;
             }
         }
