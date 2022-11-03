89783e8d07d5115cd11c94753bdf98ccbf6d5de1
kyrene chua <cwx.kyrene@gmail.com>
2022-11-03 11:11:25 +0800

    Added JUnit for ExitCommand

---
+++ src/test/java/seedu/commands/ExitCommandTest.java
@@ -0,0 +1,31 @@
+package seedu.commands;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import seedu.api.Api;
+import seedu.data.CarparkList;
+import seedu.exception.FileWriteException;
+import seedu.exception.InvalidCommandException;
+import seedu.exception.NoCarparkFoundException;
+import seedu.files.Favourite;
+import seedu.parser.Parser;
+
+public class ExitCommandTest {
+
+    @Test
+    void testExit() throws FileWriteException, NoCarparkFoundException, InvalidCommandException {
+        String input = "exit";
+        Command command = new Parser().parseCommand(input, null, null, null);
+        String result = command.execute().showToUser;
+        Assertions.assertEquals("Exiting programme.", result);
+    }
+
+    @Test
+    void testExtraParameters() throws FileWriteException, NoCarparkFoundException, InvalidCommandException {
+        String input = "exit please";
+        Command command = new Parser().parseCommand(input, null, null, null);
+        String result = command.execute().showToUser;
+        Assertions.assertEquals("There were unrecognized arguments after the `exit` command. " +
+                "Please try the `exit` command again by itself.", result);
+    }
+}
