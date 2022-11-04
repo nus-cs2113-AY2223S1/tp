3208a3199e4476f95d10fe23ab1cc88826d2fef7
insafhere <insaf@u.nus.edu>
2022-11-04 21:38:55 +0800

    changed from issues to issue(S)

--- src/main/java/seedu/duke/exceptions/InvalidOverallInputException.java
+++ src/main/java/seedu/duke/exceptions/InvalidOverallInputException.java
@@ -3,7 +3,7 @@ package seedu.duke.exceptions;
 public class InvalidOverallInputException extends Exception {
 
     public String getMessage() {
-        String message = "Please try to fix the issues and Try again!" + "\n"  ;
+        String message = "Please try to fix the issue(s) and Try again!";
         return message;
     }
 }
