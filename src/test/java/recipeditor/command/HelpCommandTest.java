f6bd62969777326d0d34c9c9e639984e716b5dec
qian <e0775289@u.nus.edu>
2022-11-04 17:31:04 +0800

    Junit test for find command

--- src/test/java/recipeditor/command/HelpCommandTest.java
+++ src/test/java/recipeditor/command/HelpCommandTest.java
@@ -1,8 +1,8 @@
 package recipeditor.command;
 
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
 import recipeditor.parser.Parser;
 
 public class HelpCommandTest {
