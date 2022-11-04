1afdc171a7691ec07ade176884d6922ea135439c
qian <e0775289@u.nus.edu>
2022-11-04 22:54:25 +0800

    Fix checkstyle errors

--- src/main/java/recipeditor/command/InvalidCommand.java
+++ src/main/java/recipeditor/command/InvalidCommand.java
@@ -5,8 +5,10 @@ public class InvalidCommand extends Command {
     public static final String COMMAND_TYPE = "/invalid";
     public static String INVALID_MESSAGE = "This is not a valid command! "
             + HelpCommand.CORRECT_FORMAT + " to view the correct syntax." + HelpCommand.HELP_MESSAGE;
-    public static String TEMPLATE_FILE_MISSING_MESSAGE = "Template file is missing! Regenerate Template File... Please try again";
-    public static String RECIPE_FILE_MISSING_MESSAGE = "Recipe File is missing! Regenerate Recipe File! Please try again!";
+    public static String TEMPLATE_FILE_MISSING_MESSAGE = "Template file is missing! "
+            + "Regenerate Template File... Please try again";
+    public static String RECIPE_FILE_MISSING_MESSAGE = "Recipe File is missing! "
+            + "Regenerate Recipe File! Please try again!";
     public static final String RECIPE_INDEX_OUT_OF_RANGE_MESSAGE = "Index specified is out of range!";
 
     public static final String INDEX_OUT_OF_RANGE_MESSAGE = "Index specified is out of range!";
