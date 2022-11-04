6a1753f60e8e9deb802759afbd7fcc7e04024d3e
redders7 <e0726220@u.nus.edu>
2022-11-04 19:33:56 +0800

    Fixed CI Checkstyle

--- src/test/java/seedu/duke/MediaTest.java
+++ src/test/java/seedu/duke/MediaTest.java
@@ -31,7 +31,7 @@ class MediaTest {
         assertEquals(0,test.rating);
         assertEquals("",test.genre);
         assertEquals(null,test.dateWatched);
-        assertEquals("",test.createFileString() );
+        assertEquals("",test.createFileString());
     }
 
     @Test
@@ -46,7 +46,6 @@ class MediaTest {
         } catch (ParseException e) {
             ex = e;
         }
-
         assertNotNull(ex);
         assertEquals(ParseException.class, ex.getClass());
     }
