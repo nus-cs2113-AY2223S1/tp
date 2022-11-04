20e4e0f3f758cf9dadbbec2b9011f554b9a12d25
GitPancaked <shawnytany24@gmail.com>
2022-11-04 16:05:09 +0800

    Formatting

--- src/test/java/computercomponentchooser/ComponentsTest.java
+++ src/test/java/computercomponentchooser/ComponentsTest.java
@@ -14,7 +14,9 @@ import computercomponentchooser.exceptions.NegativeNumberException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class ComponentsTest {
 
