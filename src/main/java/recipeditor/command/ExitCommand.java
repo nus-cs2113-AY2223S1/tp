4e3fd02771f914c0754ce8e58b8d471f079459a0
Than-Duc-Huy <than_duc_huy@outlook.com>
2022-11-05 22:36:03 +0800

    change

--- src/main/java/recipeditor/command/ExitCommand.java
+++ src/main/java/recipeditor/command/ExitCommand.java
@@ -8,6 +8,7 @@ import java.io.File;
 
 
 public class ExitCommand extends Command {
+    public static final String COMMAND_TYPE = "/exit";
     public static final String EXIT_MESSAGE = "RecipEditor ends here...";
     private static final String COMMAND_SYNTAX = "/exit";
     private static final String COMMAND_FUNCTION = "Exit recipeditor.";
