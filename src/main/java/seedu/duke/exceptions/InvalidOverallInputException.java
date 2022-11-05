aa7ce859b3c01ab0c9a6ea7d2f2dcfe2c09d0fa8
Zhai-Yuxin <e0772744@u.nus.edu>
2022-11-06 00:07:55 +0800

    solved empty row output issue

--- src/main/java/seedu/duke/exceptions/InvalidOverallInputException.java
+++ src/main/java/seedu/duke/exceptions/InvalidOverallInputException.java
@@ -3,6 +3,6 @@ package seedu.duke.exceptions;
 public class InvalidOverallInputException extends Exception {
 
     public String getMessage() {
-        return "Please try to fix the issue(s) and Try again!";
+        return "Please try to fix the issue(s) and Try again! \n";
     }
 }
