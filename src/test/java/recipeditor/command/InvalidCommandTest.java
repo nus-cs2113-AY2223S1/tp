035a10c5575f0d13af67b290b2c29b26d617ecb7
Brominne <e0773671@u.nus.edu>
2022-11-07 02:11:17 +0800

    fix InvalidCommandTest

--- src/test/java/recipeditor/command/InvalidCommandTest.java
+++ src/test/java/recipeditor/command/InvalidCommandTest.java
@@ -8,7 +8,7 @@ public class InvalidCommandTest {
 
     @Test
     public void defaultInvalidCommand_success() {
-        String actual = new InvalidCommand().execute().getMessage();
+        String actual = new InvalidCommand(InvalidCommand.INVALID_MESSAGE).execute().getMessage();
         String expected = InvalidCommand.INVALID_MESSAGE;
         assertEquals(actual, expected);
     }
