832064722f707bfd9d524dabf296c294d674fbf1
Brominne <e0773671@u.nus.edu>
2022-11-06 17:12:11 +0800

    Add Junit Test for Delete

---
+++ src/test/java/recipeditor/edit/DeleteTest.java
@@ -0,0 +1,101 @@
+package recipeditor.edit;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import recipeditor.command.InvalidCommand;
+import recipeditor.exception.InvalidFlagException;
+import recipeditor.parser.FlagType;
+import recipeditor.recipe.Ingredient;
+import recipeditor.recipe.Recipe;
+import recipeditor.recipe.RecipeList;
+import recipeditor.storage.Storage;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DeleteTest {
+    private static final String TEST_RECIPE_TITLE = "eR$^.U*0(";
+    private static Recipe recipe;
+
+    @BeforeAll
+    public static void setUp() {
+        Storage.loadRecipesToRecipeList();
+        Storage.loadRecipesToRecipeTitlesList();
+        recipe = new Recipe(TEST_RECIPE_TITLE, "test_description");
+        recipe.addIngredient(new Ingredient("ingredient", 100.0, "g"));
+        recipe.addStep("step 1");
+        recipe.addStep("finishing step ~");
+    }
+
+    @Test
+    public void delete_success() {
+        String[] parsedCommand = {"/edit", "1", "-del", "-i", "1"};
+        FlagType ingredientFlag = FlagType.INGREDIENT;
+        Delete delete = new Delete(ingredientFlag, parsedCommand, recipe);
+        try {
+            String actual = delete.execute().getIngredientAttributesFormatted();
+            String expected = new Recipe().getIngredientAttributesFormatted();
+            assertEquals(expected, actual);
+        } catch (InvalidFlagException e) {
+            assert false;
+        }
+    }
+
+    @Test
+    public void delete_invalidFlag() {
+        String[] parsedCommand = {"/edit", "1", "-del", "-k", "1"};
+        FlagType ingredientFlag = FlagType.NULL;
+        Delete delete = new Delete(ingredientFlag, parsedCommand, recipe);
+        try {
+            delete.execute();
+            assert false;
+        } catch (InvalidFlagException e) {
+            assert true;
+        }
+    }
+
+    @Test
+    public void delete_noIndex() {
+        String[] parsedCommand = {"/edit", "1", "-del", "-i"};
+        FlagType ingredientFlag = FlagType.INGREDIENT;
+        Delete delete = new Delete(ingredientFlag, parsedCommand, recipe);
+        try {
+            delete.execute();
+            assert false;
+        } catch (IndexOutOfBoundsException e) {
+            assert true;
+        } catch (InvalidFlagException e) {
+            assert false;
+        }
+    }
+
+    @Test
+    public void delete_indexOutOfBound() {
+        String[] parsedCommand = {"/edit", "1", "-del", "-s", "0"};
+        FlagType ingredientFlag = FlagType.STEP;
+        Delete delete = new Delete(ingredientFlag, parsedCommand, recipe);
+        try {
+            delete.execute();
+            assert false;
+        } catch (IndexOutOfBoundsException e) {
+            assert true;
+        } catch (InvalidFlagException e) {
+            assert false;
+        }
+    }
+
+    @Test
+    public void delete_expectIndexRecieveString() {
+        String[] parsedCommand = {"/edit", "1", "-del", "-i", "string"};
+        FlagType ingredientFlag = FlagType.INGREDIENT;
+        Delete delete = new Delete(ingredientFlag, parsedCommand, recipe);
+        try {
+            delete.execute();
+            assert false;
+        } catch (NumberFormatException e) {
+            assert true;
+        } catch (Exception e) {
+            assert false;
+        }
+    }
+
+}
