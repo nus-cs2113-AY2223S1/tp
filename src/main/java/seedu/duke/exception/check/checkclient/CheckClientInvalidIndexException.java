93fdd6529ae5dd8fbca67120c7b63824ca187657
FeliciaBeatrice <feliciabeatrice7@gmail.com>
2022-11-06 11:31:10 +0800

    Create Check Client exceptions

--- src/main/java/seedu/duke/exception/check/checkclient/CheckClientInvalidIndexException.java
+++ src/main/java/seedu/duke/exception/check/checkclient/CheckClientInvalidIndexException.java
@@ -1,7 +1,18 @@
+//@@author FeliciaBeatrice
+
 package seedu.duke.exception.check.checkclient;
 
 import seedu.duke.exception.check.CommandCheckException;
 
-public class CheckClientInvalidIndexException extends CommandCheckException {
+import static seedu.duke.Messages.MESSAGE_INVALID_INDEX;
 
+/**
+ * Representation of an exception where the user provides an index that is not within the client list in a check
+ * client command.
+ */
+public class CheckClientInvalidIndexException extends CommandCheckException {
+    @Override
+    public String toString() {
+        return MESSAGE_INVALID_INDEX;
+    }
 }
