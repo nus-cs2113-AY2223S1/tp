a48d5fa4caf75867feb7decf75734e0768b36290
qian <e0775289@u.nus.edu>
2022-11-06 16:37:57 +0800

    Update parser test

--- src/test/java/recipeditor/parser/ParserTest.java
+++ src/test/java/recipeditor/parser/ParserTest.java
@@ -89,15 +89,6 @@ class ParserTest {
     }
 
     @Test
-    void completeExitCommand_correctExitCommandFormat_ExitProgram() {
-        String input = "/exit";
-        Command commandExecuted = Parser.parseCommand(input);
-        CommandResult commandExecutedResult = commandExecuted.execute();
-        assertEquals(ExitCommand.EXIT_MESSAGE, commandExecutedResult.getMessage());
-        assertEquals(ExitCommand.class, Parser.parseCommand(input).getClass());
-    }
-
-    @Test
     void incorrectFindCommand_wrongParameter_correctFormatForFindCommand() {
         String input = "/find";
         Command commandExecuted = Parser.parseCommand(input);
