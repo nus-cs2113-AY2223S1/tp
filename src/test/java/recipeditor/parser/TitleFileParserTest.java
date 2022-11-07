066083f1e80c5b6b156c78ea5affe568a2382f8b
Than-Duc-Huy <than_duc_huy@outlook.com>
2022-11-07 09:21:10 +0800

    new workflow

--- src/test/java/recipeditor/parser/TitleFileParserTest.java
+++ src/test/java/recipeditor/parser/TitleFileParserTest.java
@@ -1,5 +1,6 @@
 package recipeditor.parser;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import recipeditor.recipe.RecipeList;
@@ -7,11 +8,26 @@ import recipeditor.recipe.RecipeList;
 import org.junit.jupiter.api.Assertions;
 import recipeditor.storage.Storage;
 
+import java.io.File;
+import java.io.FileWriter;
+
+
 public class TitleFileParserTest {
     void createFiles(String title) {
         Storage.createFile(Storage.titleToFilePath(title));
     }
 
+    void createFileWithContentAndTitle(String content, String title) {
+        String path = Storage.titleToFilePath(title);
+        new File(path);
+        try {
+            FileWriter writer = new FileWriter(path);
+            writer.write(content);
+            writer.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 
     @Test
     void parse2InvalidTitle() {
@@ -23,6 +39,7 @@ public class TitleFileParserTest {
         Assertions.assertEquals(0, RecipeList.getRecipeTitlesSize());
         for (String s : strings) {
             Storage.deleteFile(Storage.titleToFilePath(s));
+            Storage.deleteFile(Storage.titleToFilePath(s));
         }
     }
 
@@ -32,8 +49,18 @@ public class TitleFileParserTest {
         for (String s : strings) {
             createFiles(s);
         }
+        String content = "# TITLE \n"
+                + "Recipe\n\n"
+                + "# DESCRIPTION\n"
+                + "Example Description\n\n"
+                + "# INGREDIENTS  INDEX. INGREDIENT_NAME / AMOUNT / UNIT \n"
+                + "1. Example "
+                + "ingredient / 1.2 / example unit \n\n"
+                + "# STEPS INDEX. DESCRIPTION\n"
+                + "1. Example step \n";
+        createFileWithContentAndTitle(content,"Recipe");
         TitleFileParser.parseTitleFileToRecipeTitles(strings);
-        Assertions.assertEquals(0, RecipeList.getRecipeTitlesSize());
+        Assertions.assertEquals(1, RecipeList.getRecipeTitlesSize());
         for (String s : strings) {
             Storage.deleteFile(Storage.titleToFilePath(s));
             RecipeList.deleteRecipeFromTitle(s);
@@ -51,6 +78,31 @@ public class TitleFileParserTest {
         for (String s : strings) {
             createFiles(s);
         }
+        String content = "# TITLE \n"
+                + "Recipe\n\n"
+                + "# DESCRIPTION\n"
+                + "Example Description\n\n"
+                + "# INGREDIENTS  INDEX. INGREDIENT_NAME / AMOUNT / UNIT \n"
+                + "1. Example "
+                + "ingredient / 1.2 / example unit \n\n"
+                + "# STEPS INDEX. DESCRIPTION\n"
+                + "1. Example step \n";
+        createFileWithContentAndTitle(content,"Recipe");
+        TitleFileParser.parseTitleFileToRecipeTitles(strings);
+        Assertions.assertEquals(1, RecipeList.getRecipeTitlesSize());
+        for (String s : strings) {
+            Storage.deleteFile(Storage.titleToFilePath(s));
+            RecipeList.deleteRecipeFromTitle(s);
+        }
+    }
+
+    @Test
+    void parse2NoFileTitle() {
+        String[] strings = {"Recipe12", "Recipe23"};
+        for (String s : strings) {
+            Storage.deleteFile(Storage.titleToFilePath(s));
+            RecipeList.deleteRecipeFromTitle(s);
+        }
         TitleFileParser.parseTitleFileToRecipeTitles(strings);
         Assertions.assertEquals(0, RecipeList.getRecipeTitlesSize());
         for (String s : strings) {
@@ -65,20 +117,35 @@ public class TitleFileParserTest {
         for (String s : strings) {
             createFiles(s);
         }
+        String content = "# TITLE \n"
+                + "Recipe\n\n"
+                + "# DESCRIPTION\n"
+                + "Example Description\n\n"
+                + "# INGREDIENTS  INDEX. INGREDIENT_NAME / AMOUNT / UNIT \n"
+                + "1. Example "
+                + "ingredient / 1.2 / example unit \n\n"
+                + "# STEPS INDEX. DESCRIPTION\n"
+                + "1. Example step \n";
+        createFileWithContentAndTitle(content,"Recipe");
+        String content1 = "# TITLE \n"
+                + "Recipe1\n\n"
+                + "# DESCRIPTION\n"
+                + "Example Description\n\n"
+                + "# INGREDIENTS  INDEX. INGREDIENT_NAME / AMOUNT / UNIT \n"
+                + "1. Example "
+                + "ingredient / 1.2 / example unit \n\n"
+                + "# STEPS INDEX. DESCRIPTION\n"
+                + "1. Example step \n";
+        createFileWithContentAndTitle(content1,"Recipe1");
         TitleFileParser.parseTitleFileToRecipeTitles(strings);
-        Assertions.assertEquals(0, RecipeList.getRecipeTitlesSize());
+        Assertions.assertEquals(2, RecipeList.getRecipeTitlesSize());
         for (String s : strings) {
             Storage.deleteFile(Storage.titleToFilePath(s));
             RecipeList.deleteRecipeFromTitle(s);
         }
     }
 
-    @Test
-    void parse2NoFileTitle() {
-        String[] strings = {"Recipe", "Recipe1"};
-        TitleFileParser.parseTitleFileToRecipeTitles(strings);
-        Assertions.assertEquals(0, RecipeList.getRecipeTitlesSize());
-    }
+
 
 
 }
